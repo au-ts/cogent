5888c8907571802f95e12057b92e635829500b7b
Zilin Chen <Zilin.Chen@data61.csiro.au>
2016-08-05 15:24:14 +1000

    compiler: a few more desugar fixed

--- cogent/src/Cogent/Desugar.hs
+++ cogent/src/Cogent/Desugar.hs
@@ -522,9 +522,8 @@ desugarExpr (B.TE t (S.Put e [Just (f,a)])) = do
 desugarExpr (B.TE t (S.Put e (fa:fas))) = do
   t' <- typeWHNF t >>= \x -> return $ S.RT (S.TTake (Just $ P.map (fst . fromJust) fas) x)
   desugarExpr $ B.TE t $ S.Put (B.TE t' $ S.Put e [fa]) fas
-desugarExpr _ = undefined
---desugarExpr (B.Promote t e) = E <$> (Promote <$> desugarType t <*> desugarExpr e)
---desugarExpr (B.TypeErrorHappened {}) = __impossible "desugarExpr (Error)"
+desugarExpr (B.TE t (S.Upcast e)) = E <$> (Promote <$> desugarType t <*> desugarExpr e)
+desugarExpr (B.TE t (S.Widen  e)) = E <$> (Promote <$> desugarType t <*> desugarExpr e)
 
 desugarOp :: S.OpName -> Op
 desugarOp "+"   = Plus
