1cca3488b96d6caa88384ffb50da6b1a7310d7d4
Liam O'Connor <liamoc@cse.unsw.edu.au>
2016-08-14 10:11:57 +1000

    Basic renderer for documentation

--- cogent/src/Cogent/Parser.hs
+++ cogent/src/Cogent/Parser.hs
@@ -114,7 +114,7 @@ pattern = avoidInitial >>
 --            | var
 --            | Con
 
-docHunk = do whiteSpace; reservedOp "@"; manyTill anyChar newline
+docHunk = do whiteSpace; reservedOp "@"; x <- manyTill anyChar newline; whiteSpace; return x
 monotype = do avoidInitial
               t1 <- typeA1
               t2 <- optionMaybe (reservedOp "->" >> typeA1)
@@ -122,15 +122,21 @@ monotype = do avoidInitial
   where
     typeA1 = do
       x <- typeA1'
-      t2 <- optionMaybe docHunk
-      case t2 of Nothing -> return x; Just doc -> return (Documentation doc x)
+      t2 <- optionMaybe (avoidInitial >> docHunk)
+      case t2 of Nothing -> return x; Just doc -> do
+                    return (Documentation doc x)
+    typeA2 = do
+      x <- typeA2'
+      t2 <- optionMaybe (avoidInitial >> docHunk)
+      case t2 of Nothing -> return x; Just doc -> do
+                    return (Documentation doc x)
     typeA1' = do avoidInitial
                  try paramtype
-                 <|> (do t <- typeA2
+                 <|> (do t <- typeA2'
                          op <- optionMaybe takeput
                          case op of Nothing -> return t; Just f -> return (f t)
                      )
-    typeA2 = avoidInitial >>
+    typeA2' = avoidInitial >>
                ((unbox >>= \op -> atomtype >>= \at -> return (op at))
            <|>  (atomtype >>= \t -> optionMaybe bang >>= \op -> case op of Nothing -> return t; Just f -> return (f t)))
     paramtype = avoidInitial >> LocType <$> getPosition <*> (TCon <$> typeConName <*> many1 typeA2 <*> pure Writable)
@@ -278,7 +284,7 @@ kindSignature = do n <- variableName
 
 
 toplevel = do
-  docs <- concat . fromMaybe [] <$> optionMaybe (many1 docHunk)
+  docs <- unlines . fromMaybe [] <$> optionMaybe (many1 docHunk)
   p <- getPosition
   when (sourceColumn p > 1) $ fail "toplevel entries should start at column 1"
   (p,docs,) <$> (try (Include <$ reserved "include" <*> stringLiteral)
@@ -306,7 +312,6 @@ toplevel = do
 
 type Parser a t = ParsecT String t Identity a
 
-type DocString = String
 program :: Parser [(SourcePos, DocString, TopLevel LocType VarName LocExpr)] t
 program = do
   { whiteSpace
