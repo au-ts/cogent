0528dd17b9feaf6071e19889af13f31933ccf994
Zilin Chen <Zilin.Chen@nicta.com.au>
2016-11-01 03:42:47 +1100

    compiler: a more correct fix to #25

--- cogent/src/Cogent/TypeCheck/Solver.hs
+++ cogent/src/Cogent/TypeCheck/Solver.hs
@@ -416,7 +416,7 @@ bound d a@(FRecord isL) b@(F (T (TRecord jsL s)))
   | is <- M.fromList isL
   , js <- M.fromList jsL
   , M.keysSet is `S.isSubsetOf` M.keysSet js
-  , a' <- F (T (TRecord (M.toList $ M.union is js) s))
+  , a' <- F (T (TRecord (map (\(k,v) -> (k, case M.lookup k is of Nothing -> v; Just v' -> v')) jsL) s))
   = bound d a' b
 bound d a@(F (T (TRecord jsL s))) b@(FRecord isL) = bound d b a  -- symm
 bound d a@(FRecord is_) b@(FRecord js_) 
