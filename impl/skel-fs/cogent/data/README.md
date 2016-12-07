1d10c8926465ec37febef09ddcd146bfb65c2475
Partha Susarla <parthasarathi.susarlaajay@data61.csiro.au>
2016-12-07 13:42:28 +1100

    [skelfs] Add VFS interface and more structure.

--- impl/skel-fs/cogent/data/README.md
+++ impl/skel-fs/cogent/data/README.md
@@ -1,7 +1,9 @@
 This directory contains two files:
 
-* defns.txt - A file containing a list of Cogent functions that are called externally.
+* defns.txt - A file containing a list of Cogent functions that are called from the
+              'anti-quoted C'/'C' files.
 
 * types.txt - A file containing a list of external type names to Cogent's C parser.
+              This file includes all the types that aren't defined in the .ac files.
 
 The names of the files could be anything, but ensure that the `Makefile`(one level above) reflects those changes in the `DEFNS` and `TYPES` variables.
--- impl/skel-fs/cogent/lib/abstract.h
+++ impl/skel-fs/cogent/lib/abstract.h
@@ -7,11 +7,58 @@
  *
  * @TAG(NICTA_GPL)
  */
+/*
+ * abstract.h
+ * This file contains the abstract data type definitions.
+ */
 
 #ifndef _ABSTRACT_H
 #define _ABSTRACT_H
 
+#include <linux_hdrs.h>                /* In cogent/plat/linux/ */
+
+#include "abstract_vfs.h"
+
+/* Wrapper state, that needs to be carried around in addition the
+   FS State(filesystem state)
+*/
+struct WrapperState {
+        void *priv;
+        struct semaphore iop_lock;
+        struct super_block *sb;
+};
+
+typedef struct WrapperState ExState; /* External State */
+typedef struct WrapperState SkelfsState;
+
+
+/* Abstract inode structure */
+struct VfsInodeAbstract {
+        struct inode inode;
+
+        struct inode_operations inodeops;
+        struct cogent_inode_operations cogent_inodeops;
+
+        struct file_operations fileops;
+        struct cogent_file_operations cogent_fileops;
+};
+
+typedef struct VfsInodeAbstract VfsInodeAbstract;
+
+/* typedefs for convenience. TODO: Cleanup???*/
+typedef struct dir_context OSDirContext;
+typedef struct buffer_head OSBuffer;
+typedef struct page OSPage;
+typedef struct page OSPageBuffer;
+typedef struct address_space VfsMemoryMap;
+typedef struct ubi_volume_desc UbiVol;
+
+typedef void File;
+typedef void VfsStat;
+typedef void VfsIattr;
+
 #define likely(x)       __builtin_expect(!!(x), 1)
 #define unlikely(x)     __builtin_expect(!!(x), 0)
+
 
 #endif  /* _ABSTRACT_H */
