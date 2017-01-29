76679d19f47c0a7c062f24a0f345c1e50a1eb738
Zilin Chen <Zilin.Chen@nicta.com.au>
2017-01-29 23:58:21 +1100

    compiler: add comments for Post
    [skip ci]

--- cogent/src/Cogent/TypeCheck/Post.hs
+++ cogent/src/Cogent/TypeCheck/Post.hs
@@ -26,23 +26,27 @@ import Cogent.TypeCheck.Base
 import Cogent.TypeCheck.Util
 import Cogent.Util
 
-import Control.Arrow (first)
+import Control.Arrow (first, second)
 import Control.Monad
 import Control.Lens
 import Control.Monad.Except
 import Control.Monad.Writer hiding (Alt, censor)
 import qualified Data.Map as M
-import Text.PrettyPrint.ANSI.Leijen as P hiding ((<$>))
+import Text.PrettyPrint.ANSI.Leijen as P hiding ((<>), (<$>))
 
 -- import Debug.Trace
 
 type Post a = ExceptT () (WriterT [ContextualisedEW] TC) a
 
-mapW :: ([ContextualisedEW] -> [ContextualisedEW]) -> Post a -> Post a
-mapW f m = ExceptT . WriterT $ do (a, w) <- runWriterT $ runExceptT m
-                                  return (a, f w)
-
-censor = mapW
+-- NOTE: The `ExceptT` on `WriterT` monad stack works in a way which,
+-- if an error has been thrown, then later operations like `tell` or
+-- `censor` will not fire. Although that is the right definition for
+-- Monad, it doesn't serve our purposes here. Thus this `censor` function,
+-- which sort of work around the problem. Still, it doesn't change the
+-- way `>>=` works in general. As a consequence, if an error has been
+-- thrown, it still doesn't contiune logging more errors. / zilinc
+censor :: ([ContextualisedEW] -> [ContextualisedEW]) -> Post a -> Post a
+censor f m = ExceptT . WriterT $ do (a, w) <- runWriterT $ runExceptT m; return (a, f w)
 
 postT :: [ErrorContext] -> TCType -> Post RawType
 postT ctx t = do
