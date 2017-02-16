826d784eed3f8125e78040a908bd99358bfbbd80
Abdul Zreika <azre6702@uni.sydney.edu.au>
2017-02-16 17:18:17 +1100

    Basic test environment set up.
    Set up a basic framework for testing/comparing two implementations of the same file system.

---
+++ impl/tests/test_template.sh
@@ -0,0 +1,16 @@
+# Test template
+# Copy into `scripts' directory
+source ../common/common.sh
+set -e
+start_test $1 $2
+
+wd=`pwd`
+
+# Add tests here
+# Checksum format: `md5sum $wd/filename(s) >> $wd/../$2`
+###
+
+
+###
+
+end_test
