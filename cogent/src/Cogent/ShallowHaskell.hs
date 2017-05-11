be7877e90e0a64b59b7de3fb6aeae9e423f47022
Zilin Chen <Zilin.Chen@data61.csiro.au>
2017-05-11 15:16:50 +1000

    crucial fix: name capturing
    also:
    *) use Word types properly
    *) recognised more limitations
    
    [skip ci]

--- cogent/src/Cogent/ShallowHaskell.hs
+++ cogent/src/Cogent/ShallowHaskell.hs
@@ -57,17 +57,28 @@ import Prelude as P
 -- NOTE:
 -- This module assumes:
 --   *) No tag duplication in Cogent source file
+--   *) Constants and field names share the same namespace, so no duplicates allowed
 --   *) No reserved names in this module are present in source file
+--   *) No unused constants which have types which are otherwise absent in the code
 
 
-
-data ReaderGen = ReaderGen { typeStrs :: [TypeStr]
-                           , typeVars :: [TyVarName]
-                           , recoverTuples :: Bool
+data ReaderGen = ReaderGen { _typeStrs :: [TypeStr]
+                           , _typeVars :: [TyVarName]
+                           , _recoverTuples :: Bool
+                           , _localBindings :: [M.Map VarName VarName]
                            }
 
+makeLenses ''ReaderGen
+
 -- update in-scope type variables
-typarUpd typar v = v {typeVars = typar}
+typarUpd :: [TyVarName] -> ReaderGen -> ReaderGen
+typarUpd typars = set typeVars typars
+
+pushScope :: ReaderGen -> ReaderGen
+pushScope = localBindings %~ (M.empty:)
+
+addBindings :: [(VarName, VarName)] -> ReaderGen -> ReaderGen
+addBindings vs = localBindings %~ (\(h:t) -> (M.fromList vs `M.union` h):t)  -- left-biased union for shadowing
 
 data WriterGen = WriterGen { datatypes :: [HS.Decl ()]
                            }
@@ -190,26 +201,6 @@ decTypeStr (VariantStr tags) = do
   tell $ WriterGen [dec]
   return tn
 
-shallowTypesFromTable :: SG ()
-shallowTypesFromTable = do
-  ts <- asks typeStrs
-  -- partition (supertypes, subtypes)
-  let (_,sups,subs) = 
-        foldl' (\(all,sups,subs) t -> 
-                  case all of
-                    [] -> if not (t `isSubtypeOfAny` sups)  -- last iteration
-                            then ([],t `Set.insert` sups,subs)
-                            else ([],sups,t `Set.insert` subs)
-                    (hd:tl) -> if not (t `isSubtypeOfAny` all) && not (t `isSubtypeOfAny` sups)
-                                 then (tl,t `Set.insert` sups,subs)
-                                 else (tl,sups,t `Set.insert` subs)) (ts, Set.empty, Set.empty) ts
-  forM_ sups $ \t -> do  -- generate decs for all supertypes
-    decTypeStr t >>= \n -> (nominalTypes %= M.insert t n)
-  forM_ subs $ \t -> do  -- find supertypes
-    case find (t `isSubtypeStr`) sups of
-      Nothing  -> __impossible "should find a supertype"
-      Just sup -> subtypes %= (M.insert t (sup, typeStrFields sup))
-
 shallowRecTupleType :: [(FieldName, (S.Type t, Bool))] -> SG (HS.Type ())
 shallowRecTupleType fs = shallowTupleType <$> mapM shallowType (map (fst . snd) fs)
 
@@ -218,7 +209,7 @@ shallowTupleType [] = error "Record should have at least 2 fields"
 shallowTupleType ts = mkTupleT ts
 
 shallowType :: S.Type t -> SG (HS.Type ())
-shallowType (TVar v) = mkVarT . mkName . snm <$> ((!!) <$> asks typeVars <*> pure (finInt v))
+shallowType (TVar v) = mkVarT . mkName . snm <$> ((!!) <$> view typeVars <*> pure (finInt v))
 shallowType (TVarBang v) = shallowType (TVar v)
 shallowType (TCon tn ts _) = mkConT (mkName tn) <$> mapM shallowType ts
 shallowType (TFun t1 t2) = TyFun () <$> shallowType t1 <*> shallowType t2
@@ -227,7 +218,7 @@ shallowType (TString) = pure . mkTyConT $ mkName "String"
 shallowType (TSum alts) = shallowTypeWithName (TSum alts)
 shallowType (TProduct t1 t2) = mkTupleT <$> sequence [shallowType t1, shallowType t2]
 shallowType (TRecord fs s) = do
-  tuples <- asks recoverTuples
+  tuples <- view recoverTuples
   if tuples && isRecTuple (map fst fs) then
     shallowRecTupleType fs
   else
@@ -235,10 +226,10 @@ shallowType (TRecord fs s) = do
 shallowType (TUnit) = pure $ TyCon () $ Special () $ UnitCon ()
 
 shallowPrimType :: PrimInt -> HS.Type ()
-shallowPrimType U8  = mkTyConT $ mkName "Int"  -- I.TyDatatype "word" [I.AntiType "8"]
-shallowPrimType U16 = mkTyConT $ mkName "Int"  -- I.TyDatatype "word" [I.AntiType "16"]
-shallowPrimType U32 = mkTyConT $ mkName "Int"  -- I.TyDatatype "word" [I.AntiType "32"]
-shallowPrimType U64 = mkTyConT $ mkName "Int"  -- I.TyDatatype "word" [I.AntiType "64"]
+shallowPrimType U8  = mkTyConT $ mkName "Word8"
+shallowPrimType U16 = mkTyConT $ mkName "Word16"
+shallowPrimType U32 = mkTyConT $ mkName "Word32"
+shallowPrimType U64 = mkTyConT $ mkName "Word64"
 shallowPrimType Boolean = mkTyConT $ mkName "Bool"
 
 -- ----------------------------------------------------------------------------
@@ -246,6 +237,13 @@ shallowPrimType Boolean = mkTyConT $ mkName "Bool"
 --
 
 internalVar = "__shallow_v"
+
+getSafeBinder :: VarName -> SG VarName
+getSafeBinder v = do
+  bs <- view localBindings
+  if v `elem` concat (P.map M.keys bs)
+    then getSafeBinder =<< (((v ++) . show) <$> (freshInt <<+= 1))
+    else return v
 
 shallowPrimOp :: CS.Op -> [Exp ()] -> Exp ()
 -- add parens because the precendence is different from Haskell's
@@ -265,8 +263,8 @@ shallowPrimOp CS.NEq    [e1,e2] = Paren () $ InfixApp () e1 (mkVarOp $ mkName "/
 shallowPrimOp CS.BitAnd [e1,e2] = Paren () $ InfixApp () e1 (mkVarOp $ mkName ".&."   ) e2 
 shallowPrimOp CS.BitOr  [e1,e2] = Paren () $ InfixApp () e1 (mkVarOp $ mkName ".|."   ) e2 
 shallowPrimOp CS.BitXor [e1,e2] = Paren () $ InfixApp () e1 (mkVarOp $ mkName "xor"   ) e2 
-shallowPrimOp CS.LShift [e1,e2] = Paren () $ InfixApp () e1 (mkVarOp $ mkName "shiftL") e2 
-shallowPrimOp CS.RShift [e1,e2] = Paren () $ InfixApp () e1 (mkVarOp $ mkName "shiftR") e2 
+shallowPrimOp CS.LShift [e1,e2] = Paren () $ InfixApp () e1 (mkVarOp $ mkName "shiftL") (mkAppE (mkVarE $ mkName "fromIntegral") [e2]) 
+shallowPrimOp CS.RShift [e1,e2] = Paren () $ InfixApp () e1 (mkVarOp $ mkName "shiftR") (mkAppE (mkVarE $ mkName "fromIntegral") [e2]) 
 shallowPrimOp CS.Not        [e] = HS.App () (mkVarE $ mkName "not"       ) e
 shallowPrimOp CS.Complement [e] = HS.App () (mkVarE $ mkName "complement") e
 shallowPrimOp _ _ = __impossible "PrimOP arity wrong"
@@ -276,10 +274,11 @@ shallowILit n Boolean = HS.Con () . UnQual () . mkName $ if n > 0 then "True" el
 shallowILit n v = Paren () $ ExpTypeSig () (Lit () $ Int () n $ show n) (shallowPrimType v)
 
 -- makes `let p = e1 in e2'
-shallowLet :: SNat n -> Pat () -> TypedExpr t v VarName -> TypedExpr t (v :+: n) VarName -> SG (Exp ())
-shallowLet _ p e1 e2 = do
+shallowLet :: SNat n -> [(VarName, VarName)] -> Pat () -> TypedExpr t v VarName -> TypedExpr t (v :+: n) VarName -> SG (Exp ())
+shallowLet n vs p e1 e2 = do
+  __assert (toInt n == P.length vs) "n == |vs|"
   e1' <- shallowExpr e1
-  e2' <- shallowExpr e2
+  e2' <- local (addBindings vs) $ shallowExpr e2
   pure $ mkLetE [(p,e1')] e2'
 
 getRecordFieldName :: TypedExpr t v VarName -> FieldIndex -> FieldName
@@ -308,10 +307,15 @@ shallowPromote e (TSum _) = shallowExpr e
 shallowPromote e t = do
   e' <- shallowExpr e
   t' <- shallowType t
-  pure $ ExpTypeSig () e' t'
+  pure $ ExpTypeSig () (mkAppE (mkVarE $ mkName "fromIntegral") [e']) t'
 
 shallowExpr :: TypedExpr t v VarName -> SG (Exp ())
-shallowExpr (TE _ (Variable (_,v))) = pure $ mkVarE $ mkName (snm v)
+shallowExpr (TE _ (Variable (_,v))) = do
+  bs <- view localBindings
+  let v' = case M.lookup v (M.unions bs) of  -- also heap-top-biased unions
+             Nothing -> v
+             Just v' -> v'
+  pure . mkVarE . mkName $ snm v'
 shallowExpr (TE _ (Fun fn ts _)) = pure $ mkVarE $ mkName (snm fn)  -- only prints the fun name
 shallowExpr (TE _ (Op opr es)) = shallowPrimOp <$> pure opr <*> (mapM shallowExpr es)
 shallowExpr (TE _ (S.App f arg)) = mkAppE <$> shallowExpr f <*> (mapM shallowExpr [arg])
@@ -328,14 +332,18 @@ shallowExpr (TE _ (S.ILit n pt)) = pure $ shallowILit n pt
 shallowExpr (TE _ (S.SLit s)) = pure $ Lit () $ String () s s
 shallowExpr (TE _ (S.Tuple e1 e2)) = HS.Tuple () Boxed <$> mapM shallowExpr [e1,e2]
 shallowExpr (TE _ (S.Put rec fld e)) = shallowSetter rec fld <$> shallowExpr rec <*> shallowType (exprType rec) <*> shallowExpr e
-shallowExpr (TE _ (S.Let nm e1 e2)) = shallowLet s1 (mkVarP $ mkName $ snm nm) e1 e2
-shallowExpr (TE _ (S.LetBang vs nm e1 e2)) = shallowLet s1 (mkVarP $ mkName $ snm nm) e1 e2
+shallowExpr (TE _ (S.Let       nm e1 e2)) = do 
+  nm' <- getSafeBinder nm
+  shallowLet s1 [(nm,nm')] (mkVarP $ mkName $ snm nm') e1 e2
+shallowExpr (TE t (S.LetBang _ nm e1 e2)) = shallowExpr (TE t $ S.Let nm e1 e2)
 shallowExpr (TE t (S.Case e tag (_,n1,e1) (_,n2,e2))) = do
   e'  <- shallowExpr e
-  e1' <- shallowExpr e1
-  e2' <- shallowExpr e2
-  let c1 = HS.Alt () (PApp () (UnQual () $ mkName tag) [mkVarP $ mkName $ snm n1]) (UnGuardedRhs () e1') Nothing
-      c2 = HS.Alt () (mkVarP . mkName $ snm n2) (UnGuardedRhs () e2') Nothing
+  n1' <- getSafeBinder n1
+  n2' <- getSafeBinder n2
+  e1' <- local (addBindings [(n1,n1')] . pushScope) $ shallowExpr e1
+  e2' <- local (addBindings [(n2,n2')] . pushScope) $ shallowExpr e2
+  let c1 = HS.Alt () (PApp () (UnQual () $ mkName tag) [mkVarP $ mkName $ snm n1']) (UnGuardedRhs () e1') Nothing
+      c2 = HS.Alt () (mkVarP . mkName $ snm n2') (UnGuardedRhs () e2') Nothing
   pure $ HS.Case () e' [c1,c2]
 shallowExpr (TE t (Esac e)) = do
   let (TSum [(f,_)]) = exprType e
@@ -343,19 +351,25 @@ shallowExpr (TE t (Esac e)) = do
   let v = mkName $ internalVar ++ show vn
   mkAppE (Lambda () [PApp () (UnQual () . mkName $ snm f) [mkVarP v]] (mkVarE v)) <$> ((:[]) <$> shallowExpr e)
 shallowExpr (TE _ (S.If c th el)) = do
-  [c',th',el'] <- mapM shallowExpr [c, th, el]
+  c'  <- shallowExpr c
+  th' <- local pushScope $ shallowExpr th
+  el' <- local pushScope $ shallowExpr el
   pure $ HS.If () c' th' el'
 shallowExpr (TE _ (S.Take (n1,n2) rec fld e)) = do
   rec' <- shallowExpr rec
-  e' <- shallowExpr e
-  let pf = mkVarP . mkName $ snm n1  -- taken field
-      pr = mkVarP . mkName $ snm n2  -- new record
+  n1' <- getSafeBinder n1
+  n2' <- getSafeBinder n2
+  let pf = mkVarP . mkName $ snm n1'  -- taken field
+      pr = mkVarP . mkName $ snm n2'  -- new record
   f' <- shallowGetter' rec fld rec'
+  e' <- local (addBindings [(n1,n1'),(n2,n2')]) $ shallowExpr e
   pure $ mkLetE [(pr,rec'), (pf,f')] e'
-shallowExpr (TE _ (S.Split (n1,n2) e1 e2)) = 
-  let p1 = mkVarP . mkName $ snm n1
-      p2 = mkVarP . mkName $ snm n2
-   in shallowLet s2 (PTuple () Boxed [p1,p2]) e1 e2
+shallowExpr (TE _ (S.Split (n1,n2) e1 e2)) = do
+  n1' <- getSafeBinder n1
+  n2' <- getSafeBinder n2
+  let p1 = mkVarP . mkName $ snm n1'
+      p2 = mkVarP . mkName $ snm n2'
+  shallowLet s2 [(n1,n1'),(n2,n2')] (PTuple () Boxed [p1,p2]) e1 e2
 
 -- ----------------------------------------------------------------------------
 -- top-level generators
@@ -370,7 +384,7 @@ shallowTypeDef tn tvs t = do
 shallowDefinition :: Definition TypedExpr VarName -> SG [Decl ()]
 shallowDefinition (FunDef _ fn ps ti to e) =
     local (typarUpd typar) $ do
-    e' <- shallowExpr e
+    e' <- local pushScope $ shallowExpr e
     ty <- shallowType $ TFun ti to
     let sig = TypeSig () [fn'] ty
         dec = FunBind () [Match () fn' [PVar () arg0] (UnGuardedRhs () e') Nothing]
@@ -404,10 +418,31 @@ shallowConst (n, te@(TE t _)) = do
   let n' = mkName $ snm n
   pure $ [TypeSig () [n'] t', FunBind () [Match () n' [] (UnGuardedRhs () e') Nothing]]
 
+-- types in the table are not complete, some types of constants are missing
+shallowTypesFromTable :: SG ()
+shallowTypesFromTable = do
+  ts <- view typeStrs
+  -- partition (supertypes, subtypes)
+  let (_,sups,subs) = 
+        foldl' (\(all,sups,subs) t -> 
+                  case all of
+                    [] -> if not (t `isSubtypeOfAny` sups)  -- last iteration
+                            then ([],t `Set.insert` sups,subs)
+                            else ([],sups,t `Set.insert` subs)
+                    (hd:tl) -> if not (t `isSubtypeOfAny` all) && not (t `isSubtypeOfAny` sups)
+                                 then (tl,t `Set.insert` sups,subs)
+                                 else (tl,sups,t `Set.insert` subs)) (ts, Set.empty, Set.empty) ts
+  forM_ sups $ \t -> do  -- generate decs for all supertypes
+    decTypeStr t >>= \n -> (nominalTypes %= M.insert t n)
+  forM_ subs $ \t -> do  -- find supertypes
+    case find (t `isSubtypeStr`) sups of
+      Nothing  -> __impossible "should find a supertype"
+      Just sup -> subtypes %= (M.insert t (sup, typeStrFields sup))
+
 shallow :: Bool -> String -> Stage -> [Definition TypedExpr VarName] -> [S.SFConst TypedExpr] -> String -> String
 shallow tuples name stg defs consts log =
   let (decs,w) = evalRWS (runSG (shallowTypesFromTable >> ((++) <$> concatMapM shallowConst consts <*> shallowDefinitions defs)))
-                         (ReaderGen (st defs) [] tuples)
+                         (ReaderGen (st defs) [] tuples [])
                          (StateGen 0 M.empty M.empty)
       tds = datatypes w
       header = (("{-\n" ++ log ++ "\n-}\n") ++)
@@ -425,11 +460,13 @@ shallow tuples name stg defs consts log =
       importAbs s = IAbs () (NoNamespace ()) $ Ident () s
       import_bits = P.map importSym [".&.", ".|."] ++ 
                     P.map importVar ["complement", "xor", "shiftL", "shiftR"]
-      import_prelude = P.map importVar ["not", "div", "mod", "undefined"] ++
+      import_word = P.map importAbs ["Word8", "Word16", "Word32", "Word64"]
+      import_prelude = P.map importVar ["not", "div", "mod", "fromIntegral", "undefined"] ++
                        P.map importSym ["+", "-", "*", "&&", "||", ">", ">=", "<", "<=", "==", "/="] ++
                        P.map importAbs ["Char", "String", "Int"] ++
                        [IThingAll () $ Ident () "Bool"]
       imps = [ ImportDecl () (ModuleName () "Data.Bits") False False False Nothing Nothing (Just $ ImportSpecList () False import_bits)
+             , ImportDecl () (ModuleName () "Data.Word") False False False Nothing Nothing (Just $ ImportSpecList () False import_word)
              , ImportDecl () (ModuleName () "Prelude"  ) False False False Nothing Nothing (Just $ ImportSpecList () False import_prelude)
              ]
       in header $ prettyPrintStyleMode
@@ -440,6 +477,7 @@ shallow tuples name stg defs consts log =
 -- ----------------------------------------------------------------------------
 -- Below are smart constructors for Language.Haskell.Exts.Syntax
 --
+-- should have used Language.Haskell.Exts.Build instead :( / zilinc
 
 mkName :: String -> Name ()
 mkName s | P.head s `elem` ":!#$%&*+./<=>?@\\^|-~" = Symbol () s  -- roughly
