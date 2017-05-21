da2ff6f6e743864c26341168a93c75b339ab9b11
Zilin Chen <Zilin.Chen@nicta.com.au>
2017-05-22 02:50:02 +1000

    write everything, but not working atm
    the checked-in .c file is modified after generated.
    [skip ci]

--- cogent/examples/search/Search.hs
+++ cogent/examples/search/Search.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE DisambiguateRecordFields #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE TemplateHaskell #-}
@@ -13,35 +15,38 @@ import Data.Char        (isPrint)
 import Data.Foldable    (foldl')
 import Data.Monoid
 import Data.Word
+import Foreign
+import Foreign.C.String hiding (CString)
+import Foreign.C.Types
+import Foreign.Marshal.Alloc
+import Foreign.Ptr
+import Foreign.Storable
 import Test.QuickCheck hiding (Success)
 import Test.QuickCheck.Gen
+import Test.QuickCheck.Monadic
 
+import FFI
 import Search_Shallow_Desugar
 import Util
 
 spec_main = 
   case spec_find_str buffer "Cogent\NUL" of
     Nothing -> putStrLn "Not found!"
-    Just nd -> putStrLn $ key nd
-  where l1@(b10,b11,b12,b13) = readWord32 7
-        l2@(b20,b21,b22,b23) = readWord32 3
-        l3@(b30,b31,b32,b33) = l1
-        buffer = [b10,b11,b12,b13] ++ map (fromIntegral . fromEnum) "Data61" ++ [0]
-              ++ [b20,b21,b22,b23] ++ map (fromIntegral . fromEnum) "TS"     ++ [0]
-              ++ [b30,b31,b32,b33] ++ map (fromIntegral . fromEnum) "Cogent" ++ [0]
+    Just nd -> putStrLn $ (key :: Node -> CString) nd
 
 main = 
   let (R12 _ r) = find_str (R13 {p1 = (), p2 = buffer, p3 = "Cogent\NUL"} :: R13 _ _ _)
   in case r of
        None _  -> putStrLn "Not found!"
-       Some nd -> putStrLn $ key nd
+       Some nd -> putStrLn $ (key :: Node -> CString) nd
+
+buffer :: Buffer
+buffer = [b10,b11,b12,b13] ++ map (fromIntegral . fromEnum) "Data61" ++ [0]
+      ++ [b20,b21,b22,b23] ++ map (fromIntegral . fromEnum) "TS"     ++ [0]
+      ++ [b30,b31,b32,b33] ++ map (fromIntegral . fromEnum) "Cogent" ++ [0]
   where l1@(b10,b11,b12,b13) = readWord32 7
         l2@(b20,b21,b22,b23) = readWord32 3
         l3@(b30,b31,b32,b33) = l1
-        buffer = [b10,b11,b12,b13] ++ map (fromIntegral . fromEnum) "Data61" ++ [0]
-              ++ [b20,b21,b22,b23] ++ map (fromIntegral . fromEnum) "TS"     ++ [0]
-              ++ [b30,b31,b32,b33] ++ map (fromIntegral . fromEnum) "Cogent" ++ [0]
-
 
 buffer_1 = [9,0,0,0,108,105,98,115,101,112,111,108,49,13,0,0,0,108,105,98,112,114,111,116,111,98,117,102,49,48,19,0,0,0,108,105,98,112,97,110,103,111,99,97,105,114,111,45,49,46,48,45,48,24,0,0,0,108,105,98,115,116,97,114,116,117,112,45,110,111,116,105,102,105,99,97,116,105,111,110,48,10,0,0,0,108,105,98,115,111,109,98,111,107,51,20,0,0,0,108,105,98,112,121,116,104,111,110,51,46,53,45,109,105,110,105,109,97,108,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
@@ -51,7 +56,7 @@ bad_main = do
       (R12 _ r) = find_str (R13 () buffer_1 str)
   case r of
     None _  -> putStrLn "Not found!"
-    Some nd -> putStrLn $ key nd
+    Some nd -> putStrLn $ (key :: Node -> CString) nd
 
 
 pretty cs = concat $ map (\c -> let c' = (toEnum . fromIntegral $ c) in if isPrint c' then [c',','] else 'd':show c ++ ",") cs
@@ -90,15 +95,48 @@ badBufGen  ws = shuffle =<< goodBufGen ws
 keyGen :: [CString] -> Gen CString
 keyGen = elements
 
-prop_find_str_refinement = forAll (bufGen keys) $ \buf ->
-                           forAll (keyGen keys) $ \key ->
-                             spec_find_str buf key `match_results` find_str (R13 () buf key)
+prop_find_str_refinement' = forAll (bufGen keys) $ \buf ->
+                            forAll (keyGen keys) $ \key ->
+                              spec_find_str buf key `match_results` find_str (R13 () buf key)
+
+prop_find_str_refinement = monadicIO $ 
+                             forAllM (bufGen keys) $ \buf ->
+                             forAllM (keyGen keys) $ \key -> run $ do
+                               let r1 = spec_find_str buf key
+                               r2 <- cogent_find_str buf key
+                               return $ r1 == r2
 
 match_results :: Maybe Node -> R12 SysState (V17 () (R9 Word32 CString)) -> Bool
-match_results (Just n1) (R12 _ (Some n2)) = len n1 == len n2 && key n1 == key n2
+match_results (Just n1) (R12 _ (Some n2)) = (len :: Node -> Word32) n1 == (len :: Node -> Word32) n2 && (key :: Node -> CString) n1 == (key :: Node -> CString) n2
 match_results Nothing (R12 _ (None ())) = True
 match_results _ _ = False
 
+
+foreign import ccall unsafe "main_pp_inferred.c ffi_find_str"
+  c_find_str :: Ptr Ct27 -> IO (Ptr Ct29)
+
+cogent_find_str :: [Word8] -> CString -> IO (Maybe Node)
+cogent_find_str buf s = do
+  pstate <- malloc
+  pbuf   <- malloc
+  pokeArray (castPtr pbuf) buf
+  cstr <- newCString s
+  let args = Ct27 pstate pbuf cstr
+  pargs <- malloc
+  poke pargs args
+  prets <- c_find_str pargs
+  Ct29 _ (Ct28 (Ctag_t tag) none some) <- peek prets
+  -- free pstate
+  -- free pbuf
+  -- free pargs
+  if fromEnum tag == fromEnum tagEnumNone
+    then return Nothing
+    else if fromEnum tag == fromEnum tagEnumSome
+      then do
+        Ct4 l k <- peek some
+        k' <- peekCString k
+        return $ Just $ R9 l k'
+      else undefined   -- impossible!
 
 spec_find_str :: [Word8] -> CString -> Maybe Node
 spec_find_str buf s = snd $ foldl' (\(restb, found) _ -> spec_cmp_inc restb found s) (buf, Nothing) [0,1,2]
@@ -107,7 +145,7 @@ spec_cmp_inc :: [Word8] -> Maybe Node -> CString -> ([Word8], Maybe Node)
 spec_cmp_inc buf (Just n) _ = (buf, Just n)
 spec_cmp_inc buf Nothing  s = 
   case spec_deserialise_Node buf of
-    Success (n, buf') -> if s `spec_string_cmp` key n then (buf', Just n) else (buf', Nothing)
+    Success (n, buf') -> if s `spec_string_cmp` (key :: Node -> CString) n then (buf', Just n) else (buf', Nothing)
     Error   err       -> (buf, Nothing)
 
 spec_string_cmp :: CString -> CString -> Bool
