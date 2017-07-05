e926ad0101d4086144d5ea2283e4b8fe75e17bfd
Zilin Chen <Zilin.Chen@data61.csiro.au>
2017-07-05 16:20:27 +1000

    quickcheck: conv functions done
    [skip ci]

--- impl/fs/bilby/highlevel/run_ghci.sh
+++ impl/fs/bilby/highlevel/run_ghci.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # TODO: run hsc2hs first to generate FFI.hs
-ghci -i../proof/spec -i../../../../cogent/lib/gum/common -package=extra -package=QuickCheck -Wno-partial-type-signatures -package-db=.cabal-sandbox/x86_64-linux-ghc-8.0.1-packages.conf.d/
+ghci -i../proof/spec -i../highlevel -i../../../../cogent/lib/gum/common -package=extra -package=QuickCheck -Wno-partial-type-signatures -package-db=.cabal-sandbox/x86_64-linux-ghc-8.0.1-packages.conf.d/
