3227147aa73a746efa541b20c6d6b2e8fca55d2f
Zilin Chen <Zilin.Chen@data61.csiro.au>
2017-07-19 17:57:30 +1000

    qc: bug fixes

--- impl/fs/bilby/proof/spec/CogentMonad.hs
+++ impl/fs/bilby/proof/spec/CogentMonad.hs
@@ -6,7 +6,7 @@ module CogentMonad where
 
 -- import Control.Monad
 import Data.Set as S
-import Prelude hiding (return, (>>=))
+import Prelude hiding (return, (>>=), (>>))
 
 type Cogent_monad a = Set a
 
@@ -29,8 +29,13 @@ instance Functor Set where
 return :: a -> Set a
 return = singleton
   
+infixl 1 >>=, >>
+
 (>>=) :: (Ord b) => Set a -> (a -> Set b) -> Set b
 m >>= f = flatten (f `image` m)
+
+(>>) :: (Ord b) => Set a -> Set b -> Set b
+x >> y = x >>= \_ -> y
 
 select :: Set a -> Cogent_monad a
 select = id
