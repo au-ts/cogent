2753ac3c444d117ab4334aad83d17f72360e858c
Zilin Chen <Zilin.Chen@data61.csiro.au>
2017-07-25 15:51:26 +1000

    qc: fix FFI of fsm_example

--- impl/fs/bilby/highlevel/fsm_example/fsm.hs
+++ impl/fs/bilby/highlevel/fsm_example/fsm.hs
@@ -157,40 +157,40 @@ prop_fsm_init_nb_free_eb = forAll gen_MountState $ \mount_st ->
                                               Right s -> nb_free_eb s <= nb_eb (super mount_st)) rs
 
 foreign import ccall unsafe "fsm_wrapper_pp_inferred.c ffi_fsm_init"
-  c_fsm_init :: Ptr FFI.Ct432 -> IO (Ptr FFI.Cffi_fsm_init_ds)
+  c_fsm_init :: Ptr FFI.Ct21 -> IO (Ptr FFI.Cffi_fsm_init_ds)
 
 release_fsm_init :: Either ErrCode FsmState -> IO ()
 release_fsm_init (Left _) = return ()
-release_fsm_init (Right r) = conv_FsmState r >>= new >>= c_destroy_Ct68
+release_fsm_init (Right r) = conv_FsmState r >>= new >>= c_destroy_Ct20
 
-foreign import ccall unsafe "fsm_wrapper_pp_inferred.c ffi_destroy_Ct68"
-  c_destroy_Ct68 :: Ptr FFI.Ct68 -> IO ()
+foreign import ccall unsafe "fsm_wrapper_pp_inferred.c ffi_destroy_Ct20"
+  c_destroy_Ct20 :: Ptr FFI.Ct20 -> IO ()
 
-conv_ObjSuper :: ObjSuper -> IO FFI.Ct39
+conv_ObjSuper :: ObjSuper -> IO FFI.Ct9
 conv_ObjSuper (R26 {..}) = 
-  return $ FFI.Ct39 { FFI.nb_eb           = fromIntegral nb_eb
-                    , FFI.eb_size         = fromIntegral eb_size
-                    , FFI.io_size         = fromIntegral io_size
-                    , FFI.nb_reserved_gc  = fromIntegral nb_reserved_gc
-                    , FFI.nb_reserved_del = fromIntegral nb_reserved_del
-                    , FFI.cur_eb          = fromIntegral cur_eb
-                    , FFI.cur_offs        = fromIntegral cur_offs
-                    , FFI.last_inum       = fromIntegral last_inum
-                    , FFI.next_sqnum      = fromIntegral next_sqnum
-                    }
+  return $ FFI.Ct9 { FFI.nb_eb           = fromIntegral nb_eb
+                   , FFI.eb_size         = fromIntegral eb_size
+                   , FFI.io_size         = fromIntegral io_size
+                   , FFI.nb_reserved_gc  = fromIntegral nb_reserved_gc
+                   , FFI.nb_reserved_del = fromIntegral nb_reserved_del
+                   , FFI.cur_eb          = fromIntegral cur_eb
+                   , FFI.cur_offs        = fromIntegral cur_offs
+                   , FFI.last_inum       = fromIntegral last_inum
+                   , FFI.next_sqnum      = fromIntegral next_sqnum
+                   }
 
-conv_ObjData :: ObjData -> IO FFI.Ct62
+conv_ObjData :: ObjData -> IO FFI.Ct10
 conv_ObjData (R21 {..}) = do
   p_odata <- new =<< conv_WordArray (return . fromIntegral) odata
-  return $ FFI.Ct62 (fromIntegral id) p_odata
+  return $ FFI.Ct10 (fromIntegral id) p_odata
 
-conv_ObjDel :: ObjDel -> IO FFI.Ct63
-conv_ObjDel (R19 x) = return $ FFI.Ct63 $ fromIntegral x
+conv_ObjDel :: ObjDel -> IO FFI.Ct11
+conv_ObjDel (R19 x) = return $ FFI.Ct11 $ fromIntegral x
 
-conv_ObjDentry :: ObjDentry -> IO FFI.Ct48
+conv_ObjDentry :: ObjDentry -> IO FFI.Ct12
 conv_ObjDentry (R24 {..}) = do
   p_name <- new =<< conv_WordArray (return . fromIntegral) name
-  return $ FFI.Ct48 { FFI.ino   = fromIntegral ino
+  return $ FFI.Ct12 { FFI.ino   = fromIntegral ino
                     , FFI.dtype = fromIntegral dtype
                     , FFI.nlen  = fromIntegral nlen
                     , FFI.name  = p_name
@@ -202,17 +202,17 @@ conv_Array f xs = do
   p_p_values <- new p_values
   return $ FFI.CArray (CInt $ fromIntegral $ length xs) p_p_values
 
-conv_ObjDentarr :: ObjDentarr -> IO FFI.Ct64
+conv_ObjDentarr :: ObjDentarr -> IO FFI.Ct13
 conv_ObjDentarr (R20 {..}) = do
   p_entries <- new =<< conv_Array conv_ObjDentry entries
-  return $ FFI.Ct64 { id = fromIntegral id
+  return $ FFI.Ct13 { id = fromIntegral id
                     , nb_dentry = fromIntegral nb_dentry
                     , entries = p_entries
                     }
 
-conv_ObjInode :: ObjInode -> IO FFI.Ct45
+conv_ObjInode :: ObjInode -> IO FFI.Ct14
 conv_ObjInode (R22 {..}) = 
-  return $ FFI.Ct45 { FFI.id        = fromIntegral id
+  return $ FFI.Ct14 { FFI.id        = fromIntegral id
                     , FFI.size      = fromIntegral size
                     , FFI.atime_sec = fromIntegral atime_sec
                     , FFI.ctime_sec = fromIntegral ctime_sec
@@ -227,33 +227,33 @@ conv_ObjInode (R22 {..}) =
 conv_WordArray :: (Storable t') => (t -> IO t') -> WordArray t -> IO (FFI.CWordArray t')
 conv_WordArray f xs = FFI.CWordArray (fromIntegral $ length xs) <$> (newArray =<< mapM f xs)
 
-conv_ObjSumEntry :: ObjSumEntry -> IO (FFI.Ct10)
+conv_ObjSumEntry :: ObjSumEntry -> IO (FFI.Ct15)
 conv_ObjSumEntry (R23 {..}) = 
-  return $ FFI.Ct10 { FFI.id    = fromIntegral id
+  return $ FFI.Ct15 { FFI.id    = fromIntegral id
                     , FFI.sqnum = fromIntegral sqnum
                     , FFI.len   = fromIntegral len
                     , FFI.del_flags_and_offs = fromIntegral del_flags_and_offs
                     , FFI.count = fromIntegral count
                     }
 
-conv_ObjSummary :: ObjSummary -> IO FFI.Ct42
+conv_ObjSummary :: ObjSummary -> IO FFI.Ct16
 conv_ObjSummary (R28 {..}) = do
   p_entries <- new =<< conv_WordArray conv_ObjSumEntry entries
-  return $ FFI.Ct42 { FFI.nb_sum_entry = fromIntegral nb_sum_entry
+  return $ FFI.Ct16 { FFI.nb_sum_entry = fromIntegral nb_sum_entry
                     , FFI.entries      = p_entries
                     , FFI.sum_offs     = fromIntegral sum_offs
                     }
 
-conv_ObjUnion :: ObjUnion -> IO FFI.Ct65
+conv_ObjUnion :: ObjUnion -> IO FFI.Ct17
 conv_ObjUnion ounion = do
-  let def_data    = FFI.Ct62 {id = 0, odata = nullPtr}
-      def_del     = FFI.Ct63 {id = 0}
+  let def_data    = FFI.Ct10 {id = 0, odata = nullPtr}
+      def_del     = FFI.Ct11 {id = 0}
       def_dentarr = nullPtr
       def_inode   = nullPtr
       def_pad     = const_unit
       def_summary = nullPtr
       def_super   = nullPtr
-      o = FFI.Ct65 undefined def_data def_del def_dentarr def_inode def_pad def_summary def_super
+      o = FFI.Ct17 undefined def_data def_del def_dentarr def_inode def_pad def_summary def_super
   case ounion of
     TObjData    t -> conv_ObjData    t         >>= \x -> return $ o { FFI.tag = FFI.Ctag_t $ fromIntegral $ fromEnum FFI.tag_ENUM_TObjData   , FFI.tObjData    = x }
     TObjDel     t -> conv_ObjDel     t         >>= \x -> return $ o { FFI.tag = FFI.Ctag_t $ fromIntegral $ fromEnum FFI.tag_ENUM_TObjDel    , FFI.tObjDel     = x }
@@ -263,10 +263,10 @@ conv_ObjUnion ounion = do
     TObjSummary t -> conv_ObjSummary t >>= new >>= \x -> return $ o { FFI.tag = FFI.Ctag_t $ fromIntegral $ fromEnum FFI.tag_ENUM_TObjSummary, FFI.tObjSummary = x }
     TObjSuper   t -> conv_ObjSuper   t >>= new >>= \x -> return $ o { FFI.tag = FFI.Ctag_t $ fromIntegral $ fromEnum FFI.tag_ENUM_TObjSuper  , FFI.tObjSuper   = x }
 
-conv_Obj :: Obj -> IO FFI.Ct66
+conv_Obj :: Obj -> IO FFI.Ct18
 conv_Obj (R25 {..}) = do
   ounion' <- conv_ObjUnion ounion
-  return $ FFI.Ct66 { FFI.magic  = fromIntegral magic
+  return $ FFI.Ct18 { FFI.magic  = fromIntegral magic
                     , FFI.crc    = fromIntegral crc
                     , FFI.sqnum  = fromIntegral sqnum
                     , FFI.offs   = fromIntegral offs
@@ -281,13 +281,13 @@ conv_UbiVolInfo = return
 conv_UbiDevInfo :: UbiDevInfo -> IO FFI.CUbiDevInfo
 conv_UbiDevInfo = return
 
-conv_MountState :: MountState -> IO FFI.Ct72
+conv_MountState :: MountState -> IO FFI.Ct19
 conv_MountState (R11 {..}) = do
   p_super   <- new =<< conv_ObjSuper super
   p_obj_sup <- new =<< conv_Obj obj_sup
   p_vol     <- new =<< conv_UbiVolInfo vol
   p_dev     <- new =<< conv_UbiDevInfo dev
-  return $ FFI.Ct72 { eb_recovery      = fromIntegral eb_recovery
+  return $ FFI.Ct19 { eb_recovery      = fromIntegral eb_recovery
                     , eb_recovery_offs = fromIntegral eb_recovery_offs
                     , super            = p_super
                     , obj_sup          = p_obj_sup
@@ -297,64 +297,64 @@ conv_MountState (R11 {..}) = do
                     , no_summary       = FFI.Cbool_t $ CUChar $ fromIntegral $ fromEnum no_summary
                     }
 
-conv_GimNode :: GimNode -> IO FFI.Ct18
-conv_GimNode (R10 {..}) = return $ FFI.Ct18 (fromIntegral count) (fromIntegral sqnum)
+conv_GimNode :: GimNode -> IO FFI.Ct3
+conv_GimNode (R10 {..}) = return $ FFI.Ct3 (fromIntegral count) (fromIntegral sqnum)
 
 -- Rbt is not refined
 conv_Rbt :: (Storable k', Storable v') => (k -> IO k') -> (v -> IO v') -> Rbt k v -> IO (FFI.CRbt k' v')
 conv_Rbt fk fv t = ttraverse fk =<< traverse fv t
 
-conv_FsmState :: FsmState -> IO FFI.Ct68
+conv_FsmState :: FsmState -> IO FFI.Ct20
 conv_FsmState (R27 {..}) = do
   p_used_eb     <- new =<< conv_WordArray (return . fromIntegral) used_eb
   p_dirty_space <- new =<< conv_WordArray (return . fromIntegral) dirty_space
   p_gim         <- new =<< conv_Rbt (return . fromIntegral) conv_GimNode gim
-  return $ FFI.Ct68 { nb_free_eb  = fromIntegral nb_free_eb
+  return $ FFI.Ct20 { nb_free_eb  = fromIntegral nb_free_eb
                     , used_eb     = p_used_eb
                     , dirty_space = p_dirty_space
                     , gim         = p_gim
                     }
 
-mk_fsm_init_arg :: MountState -> FsmState -> IO FFI.Ct432
+mk_fsm_init_arg :: MountState -> FsmState -> IO FFI.Ct21
 mk_fsm_init_arg mount_st fsm_st = do
   p_sys_st   <- pDummyCSysState
   p_mount_st <- new =<< conv_MountState mount_st
   p_fsm_st   <- new =<< conv_FsmState fsm_st
-  return $ FFI.Ct432 { p1 = p_sys_st, p2 = p_mount_st, p3 = p_fsm_st }
+  return $ FFI.Ct21 { p1 = p_sys_st, p2 = p_mount_st, p3 = p_fsm_st }
 
-conv_Ct433 :: FFI.Ct433 -> IO ErrCode
-conv_Ct433 (FFI.Ct433 {..}) = return $ fromIntegral p1
+conv_Ct22 :: FFI.Ct22 -> IO ErrCode
+conv_Ct22 (FFI.Ct22 {..}) = return $ fromIntegral p1
 
 conv_CWordArray :: (Storable t) => (t -> IO t') -> FFI.CWordArray t -> IO (WordArray t')
 conv_CWordArray f (FFI.CWordArray {..}) = mapM f =<< peekArray (fromIntegral len) values
 
-conv_Ct18 :: FFI.Ct18 -> IO GimNode
-conv_Ct18 (FFI.Ct18 {..}) = return $ R10 (fromIntegral count) (fromIntegral sqnum)
+conv_Ct3 :: FFI.Ct3 -> IO GimNode
+conv_Ct3 (FFI.Ct3 {..}) = return $ R10 (fromIntegral count) (fromIntegral sqnum)
 
 conv_CRbt :: (Storable k, Storable v) => (k -> IO k') -> (v -> IO v') -> FFI.CRbt k v -> IO (Rbt k' v')
 conv_CRbt fk fv t = ttraverse fk =<< traverse fv t
 
-conv_Ct68 :: FFI.Ct68 -> IO FsmState
-conv_Ct68 (FFI.Ct68 {..}) = do
+conv_Ct20 :: FFI.Ct20 -> IO FsmState
+conv_Ct20 (FFI.Ct20 {..}) = do
   p_used_eb     <- peek used_eb     >>= conv_CWordArray (return . fromIntegral)
   p_dirty_space <- peek dirty_space >>= conv_CWordArray (return . fromIntegral)
-  p_gim         <- peek gim         >>= conv_CRbt (return . fromIntegral) conv_Ct18
+  p_gim         <- peek gim         >>= conv_CRbt (return . fromIntegral) conv_Ct3
   return $ R27 (fromIntegral nb_free_eb) p_used_eb p_dirty_space p_gim
 
-conv_Ct434 :: FFI.Ct434 -> IO (Either ErrCode FsmState)
-conv_Ct434 (FFI.Ct434 {..}) = do
+conv_Ct23 :: FFI.Ct23 -> IO (Either ErrCode FsmState)
+conv_Ct23 (FFI.Ct23 {..}) = do
   let FFI.Ctag_t t = tag
-  if | fromIntegral t == fromEnum FFI.tag_ENUM_Error   -> conv_Ct433 error >>= return . Left
-     | fromIntegral t == fromEnum FFI.tag_ENUM_Success -> (conv_Ct68 =<< peek success) >>= return . Right
+  if | fromIntegral t == fromEnum FFI.tag_ENUM_Error   -> conv_Ct22 error >>= return . Left
+     | fromIntegral t == fromEnum FFI.tag_ENUM_Success -> (conv_Ct20 =<< peek success) >>= return . Right
      | otherwise -> Prelude.error $ "Tag is " ++ show (fromIntegral t)
 
-conv_Ct435 :: FFI.Ct435 -> IO (Either ErrCode FsmState)
-conv_Ct435 (FFI.Ct435 {..}) = conv_Ct434 p2
+conv_Ct24 :: FFI.Ct24 -> IO (Either ErrCode FsmState)
+conv_Ct24 (FFI.Ct24 {..}) = conv_Ct23 p2
 
 mk_fsm_init_ret :: FFI.Cffi_fsm_init_ds -> IO (Either ErrCode FsmState, [Bool])
 mk_fsm_init_ret (FFI.Cffi_fsm_init_ds p_ret p_ds) = do
   ret <- peek p_ret
-  ret' <- conv_Ct435 ret
+  ret' <- conv_Ct24 ret
   ds'  <- peekArray 2 p_ds
   return $ (ret', ds')
 
