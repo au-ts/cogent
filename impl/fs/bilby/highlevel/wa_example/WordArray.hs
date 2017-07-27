abe7604d3dc3a68e1f0205e1bf5cf00086e2bc08
Zilin Chen <Zilin.Chen@data61.csiro.au>
2017-07-27 13:28:15 +1000

    qc: get a HOF work

--- impl/fs/bilby/highlevel/wa_example/WordArray.hs
+++ impl/fs/bilby/highlevel/wa_example/WordArray.hs
@@ -14,6 +14,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {- LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 
 {-# OPTIONS_GHC -Wno-missing-fields #-}
@@ -34,6 +35,7 @@ import Foreign.Storable
 import Prelude as P
 import Test.QuickCheck hiding (Success)
 import Test.QuickCheck.Arbitrary
+import Test.QuickCheck.Function
 import Test.QuickCheck.Gen
 import Test.QuickCheck.Monadic
 
@@ -49,6 +51,16 @@ import Util
 
 ifThenElse c e1 e2 = case c of True -> e1; False -> e2
 
+wordarray_u8_corres :: WordArray Word8 -> CWordArray Cu8 -> IO Bool
+wordarray_u8_corres hs_arr c_arr = do
+  arr <- peekArray (fromIntegral $ len c_arr) (values c_arr)
+  return $ arr == P.map fromIntegral hs_arr
+
+toC_wordarray_u8 :: WordArray Word8 -> IO (CWordArray Cu8)
+toC_wordarray_u8 hs_arr = do
+  p_vals <- newArray $ P.map fromIntegral hs_arr
+  let len = length hs_arr
+  return $ CWordArray (fromIntegral len) p_vals
 
 -- /////////////////////////////////////////////////////////////////////////////
 
@@ -77,19 +89,14 @@ wordarray_create_u8_corres :: Maybe (WordArray Word8) -> Ct5 -> IO Bool
 wordarray_create_u8_corres Nothing    (Ct5 {..}) | fromEnum tag == fromEnum tagEnumError = pure True
 wordarray_create_u8_corres (Just arr) (Ct5 {..}) | fromEnum tag == fromEnum tagEnumSuccess = do
   let Ct3 _ p_arr = success
-  c_arr <- peek p_arr
-  hs_arr <- peekArray (fromIntegral $ len c_arr) (values c_arr)
-  return $ hs_arr == P.map fromIntegral arr
+  wordarray_u8_corres arr =<< peek p_arr
 wordarray_create_u8_corres _ _ = return False
 
 foreign import ccall unsafe "ffi_wordarray_create_u8"
   c_wordarray_create_u8 :: Ptr Ct4 -> IO (Ptr Ct5)
 
 cogent_wordarray_create_u8 :: Ct4 -> IO Ct5
-cogent_wordarray_create_u8 arg = do
-  p_arg <- new arg
-  p_ret <- c_wordarray_create_u8 p_arg
-  peek p_ret
+cogent_wordarray_create_u8 arg = peek =<< c_wordarray_create_u8 =<< new arg
 
 hs_wordarray_create :: (Integral a) => Word32 -> Cogent_monad (Maybe (WordArray a))
 hs_wordarray_create l = (return . Just $ replicate (fromIntegral l) (fromIntegral 0)) `alternative` (return Nothing)
@@ -108,17 +115,11 @@ prop_wordarray_get_u8_corres = monadicIO $
 gen_wordarray_get_u8_arg :: Gen (R15 (WordArray Word8) Word32)
 gen_wordarray_get_u8_arg = do
   arr <- listOf (arbitrary :: Gen Word8)
-  let len = length arr
-  idx <- elements [0 .. 2 * fromIntegral len]
+  idx <- elements [0 .. 2 * fromIntegral (length arr)]
   return $ R15 arr idx
 
 toC_wordarray_get_u8_arg :: R15 (WordArray Word8) Word32 -> IO Ct2
-toC_wordarray_get_u8_arg (R15 p1 p2) = do
-  p_vals <- newArray $ P.map fromIntegral p1
-  let len = length p1
-      cogent_arr = CWordArray (fromIntegral len) p_vals
-  p_arr <- new cogent_arr
-  return $ Ct2 p_arr (fromIntegral p2)
+toC_wordarray_get_u8_arg (R15 p1 p2) = Ct2 <$> (new =<< toC_wordarray_u8 p1) <*> (pure $ fromIntegral p2)
 
 abs_wordarray_get_u8_arg :: R15 (WordArray Word8) Word32 -> (WordArray Word8, Word32)
 abs_wordarray_get_u8_arg (R15 p1 p2) = (p1, p2)
@@ -137,6 +138,66 @@ hs_wordarray_get xs i | is_inbound xs i = xs !! (fromIntegral i)
                       | otherwise = 0
 
 -- /////////////////////////////////////////////////////////////////////////////
+-- instantiated with [U8, U8, Bool]
+
+prop_wordarray_modify_u8_corres = monadicIO $
+  forAllM gen_wordarray_modify_u8_arg $ \arg -> run $ do
+    rc <- cogent_wordarray_modify_u8 =<< toC_wordarray_modify_u8_arg arg
+    let (arr,idx,f,acc,obsv) = abs_wordarray_modify_u8_arg arg
+    ra <- return $ hs_wordarray_modify arr idx f acc obsv
+    corresM' wordarray_modify_u8_corres ra rc
+
+gen_wordarray_modify_u8_arg :: Gen (R3 (WordArray Word8) Word32 FuncEnum Word8 Bool)
+gen_wordarray_modify_u8_arg = do
+  arr  <- listOf (arbitrary :: Gen Word8)
+  idx  <- elements [0 .. 2 * fromIntegral (length arr)]
+  f    <- elements [funEnumModifyBodyF]
+  acc  <- arbitrary
+  obsv <- arbitrary
+  return $ R3 arr idx f acc obsv
+
+hs_modify_body_f :: Word8 -> Word8 -> Bool -> (Word8, Word8)
+hs_modify_body_f elem acc obsv =
+  if obsv then (elem + acc, elem + acc)
+          else (elem, acc)
+
+toC_wordarray_modify_u8_arg :: R3 (WordArray Word8) Word32 FuncEnum Word8 Bool -> IO Ct19
+toC_wordarray_modify_u8_arg (R3 {..}) = do
+  c_arr  <- new =<< toC_wordarray_u8 arr
+  c_idx  <- return $ fromIntegral idx
+  c_f    <- return $ toEnum $ fromEnum f
+  c_acc  <- return $ fromIntegral acc
+  c_obsv <- return $ if obsv then const_true else const_false
+  return $ Ct19 c_arr c_idx c_f c_acc c_obsv
+
+abs_wordarray_modify_u8_arg :: R3 (WordArray Word8) Word32 FuncEnum Word8 Bool
+                            -> (WordArray Word8, Word32, (Word8, Word8, Bool) -> (Word8, Word8), Word8, Bool)
+abs_wordarray_modify_u8_arg (R3 {..}) = 
+  let f' = if | fromEnum f == fromEnum funEnumModifyBodyF -> hs_modify_body_f
+      f'' (a1,a2,a3) = f' a1 a2 a3
+   in (arr, idx, f'', acc, obsv)
+
+wordarray_modify_u8_corres :: (WordArray Word8, Word8) -> Ct20 -> IO Bool
+wordarray_modify_u8_corres (hs_arr, hs_acc) (Ct20 p_arr acc) = do 
+  b1 <- wordarray_u8_corres hs_arr =<< peek p_arr
+  b2 <- return $ hs_acc == fromIntegral acc
+  return $ b1 && b2
+
+foreign import ccall unsafe "ffi_wordarray_modify_u8"
+  c_wordarray_modify_u8 :: Ptr Ct19 -> IO (Ptr Ct20)
+
+cogent_wordarray_modify_u8 :: Ct19 -> IO Ct20
+cogent_wordarray_modify_u8 arg = peek =<< c_wordarray_modify_u8 =<< new arg
+
+hs_wordarray_modify :: WordArray a -> Word32 -> ((a, acc, obsv) -> (a, acc)) -> acc -> obsv -> (WordArray a, acc)
+hs_wordarray_modify xs i f acc obsv
+  | is_inbound xs i = 
+    let (xs1,x:xs2) = splitAt (fromIntegral i) xs
+        (x',acc') = f (x,acc,obsv)
+     in (xs1 ++ x':xs2, acc')
+  | otherwise = (xs, acc)                          
+
+-- /////////////////////////////////////////////////////////////////////////////
 
 hs_wordarray_free :: WordArray a -> ()
 hs_wordarray_free _ = ()
@@ -145,12 +206,6 @@ hs_wordarray_get_bounded :: Integral a => WordArray a -> Word32 -> Maybe a
 hs_wordarray_get_bounded xs i =
   if is_inbound xs i then Just $ hs_wordarray_get xs i
                      else Nothing
-
-hs_wordarray_modify :: WordArray a -> Word32 -> ((a, acc, obsv) -> (a, acc)) -> acc -> obsv -> (WordArray a, acc)
-hs_wordarray_modify xs i f acc obsv = 
-  let (xs1,x:xs2) = splitAt (fromIntegral i) xs
-      (x',acc') = f (x,acc,obsv)
-   in (xs1 ++ x':xs2, acc')
 
 is_inbound :: WordArray a -> Word32 -> Bool
 is_inbound xs i = i < (fromIntegral $ length xs)
