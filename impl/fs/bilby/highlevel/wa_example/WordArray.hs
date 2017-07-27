a0f2b181928e7797a23fa7c0c97fae849c851ffd
Zilin Chen <Zilin.Chen@data61.csiro.au>
2017-07-27 12:09:29 +1000

    qc: get 2 corres test

--- impl/fs/bilby/highlevel/wa_example/WordArray.hs
+++ impl/fs/bilby/highlevel/wa_example/WordArray.hs
@@ -23,6 +23,7 @@ module WordArray where
 
 -- import Control.Monad.State
 import Data.Either.Extra  -- extra-1.6
+-- import Data.Function
 import Data.Set as S
 import Foreign
 import Foreign.C.String hiding (CString)
@@ -48,25 +49,38 @@ import Util
 
 ifThenElse c e1 e2 = case c of True -> e1; False -> e2
 
-prop_wordarray_create_corres =
-  forAllM (arbitrary :: Gen (R15 SysState Word32)) $ \arg -> run $ do
-    rc <- cogent_wordarray_create_u8 (toC_wordarray_create_u8_arg arg)
-    ra <- return $ hs_wordarray_create (abs_wordarray_create_u8_arg arg)
-    corresM wordarray_create_corres ra rc
 
-toC_wordarray_create_u8_arg :: R15 SysState Word32 -> Ct4
-toC_wordarray_create_u8_arg = undefined
+-- /////////////////////////////////////////////////////////////////////////////
+
+prop_wordarray_create_u8_corres = monadicIO $
+  forAllM gen_wordarray_create_u8_arg $ \arg -> run $ do
+    rc <- cogent_wordarray_create_u8 =<< toC_wordarray_create_u8_arg arg
+    ra <- return $ hs_wordarray_create (abs_wordarray_create_u8_arg arg)
+    corresM wordarray_create_u8_corres ra rc
+
+gen_wordarray_create_u8_arg :: Gen (R15 SysState Word32)
+gen_wordarray_create_u8_arg = do
+  s <- arbitrary
+  l <- elements [0..100]
+  return $ R15 s l
+
+toC_wordarray_create_u8_arg :: R15 SysState Word32 -> IO Ct4
+toC_wordarray_create_u8_arg (R15 p1 p2) = do
+  p1' <- pDummyCSysState
+  p2' <- return $ fromIntegral p2
+  return $ Ct4 p1' p2'
 
 abs_wordarray_create_u8_arg :: R15 SysState Word32 -> Word32
 abs_wordarray_create_u8_arg (R15 _ p2) = p2
 
-wordarray_create_corres :: Maybe (WordArray Word8) -> Ct5 -> IO Bool
-wordarray_create_corres Nothing    (Ct5 {..}) | fromEnum tag == fromEnum tagEnumError = pure True
-wordarray_create_corres (Just arr) (Ct5 {..}) | fromEnum tag == fromEnum tagEnumSuccess = do
+wordarray_create_u8_corres :: Maybe (WordArray Word8) -> Ct5 -> IO Bool
+wordarray_create_u8_corres Nothing    (Ct5 {..}) | fromEnum tag == fromEnum tagEnumError = pure True
+wordarray_create_u8_corres (Just arr) (Ct5 {..}) | fromEnum tag == fromEnum tagEnumSuccess = do
   let Ct3 _ p_arr = success
   c_arr <- peek p_arr
   hs_arr <- peekArray (fromIntegral $ len c_arr) (values c_arr)
   return $ hs_arr == P.map fromIntegral arr
+wordarray_create_u8_corres _ _ = return False
 
 foreign import ccall unsafe "ffi_wordarray_create_u8"
   c_wordarray_create_u8 :: Ptr Ct4 -> IO (Ptr Ct5)
@@ -83,12 +97,49 @@ hs_wordarray_create l = (return . Just $ replicate (fromIntegral l) (fromIntegra
         (>>=)  = (CogentMonad.>>=)
         (>>)   = (CogentMonad.>>)
 
-hs_wordarray_free :: WordArray a -> ()
-hs_wordarray_free _ = ()
+-- /////////////////////////////////////////////////////////////////////////////
+
+prop_wordarray_get_u8_corres = monadicIO $
+  forAllM gen_wordarray_get_u8_arg $ \arg -> run $ do
+    rc <- cogent_wordarray_get_u8 =<< toC_wordarray_get_u8_arg arg
+    ra <- return $ uncurry hs_wordarray_get (abs_wordarray_get_u8_arg arg)
+    return $ corres' wordarray_get_u8_corres ra rc
+
+gen_wordarray_get_u8_arg :: Gen (R15 (WordArray Word8) Word32)
+gen_wordarray_get_u8_arg = do
+  arr <- listOf (arbitrary :: Gen Word8)
+  let len = length arr
+  idx <- elements [0 .. 2 * fromIntegral len]
+  return $ R15 arr idx
+
+toC_wordarray_get_u8_arg :: R15 (WordArray Word8) Word32 -> IO Ct2
+toC_wordarray_get_u8_arg (R15 p1 p2) = do
+  p_vals <- newArray $ P.map fromIntegral p1
+  let len = length p1
+      cogent_arr = CWordArray (fromIntegral len) p_vals
+  p_arr <- new cogent_arr
+  return $ Ct2 p_arr (fromIntegral p2)
+
+abs_wordarray_get_u8_arg :: R15 (WordArray Word8) Word32 -> (WordArray Word8, Word32)
+abs_wordarray_get_u8_arg (R15 p1 p2) = (p1, p2)
+
+wordarray_get_u8_corres :: Word8 -> Cu8 -> Bool
+wordarray_get_u8_corres ra rc = fromIntegral ra == fromIntegral rc
+
+foreign import ccall unsafe "ffi_wordarray_get_0"
+  c_wordarray_get_u8 :: Ptr Ct2 -> IO Cu8
+
+cogent_wordarray_get_u8 :: Ct2 -> IO Cu8
+cogent_wordarray_get_u8 = (c_wordarray_get_u8 =<<) . new
 
 hs_wordarray_get :: Integral a => WordArray a -> Word32 -> a
 hs_wordarray_get xs i | is_inbound xs i = xs !! (fromIntegral i)
                       | otherwise = 0
+
+-- /////////////////////////////////////////////////////////////////////////////
+
+hs_wordarray_free :: WordArray a -> ()
+hs_wordarray_free _ = ()
 
 hs_wordarray_get_bounded :: Integral a => WordArray a -> Word32 -> Maybe a
 hs_wordarray_get_bounded xs i =
