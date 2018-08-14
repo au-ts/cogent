b2e24148a747fd4722da82fecd4a788ef38d294e
vjackson725 <vjackson725@users.noreply.github.com>
2018-08-14 16:28:46 +1000

    proof: actually add ML_Old.thy

---
+++ cogent/isa/ML_Old.thy
@@ -0,0 +1,47 @@
+(*
+  FIXME:
+
+  This file re-implements old (deprecated!) methods that were used in the ML parts of the Cogent
+  proofs from circa 2015. Eventually we should rewrite these proofs in a more modern style.
+
+*)
+theory ML_Old
+  imports Pure
+begin
+  ML {*
+  
+  val term_pat_setup =
+  let
+    val name_inner_syntax = Args.name_token >> Token.inner_syntax_of
+    val parser = Args.context -- Scan.lift name_inner_syntax
+  
+    fun term_pat (ctxt, str) =
+      str |> Proof_Context.read_term_pattern ctxt
+          |> ML_Syntax.print_term
+          |> ML_Syntax.atomic
+  
+  in
+    ML_Antiquotation.inline @{binding "term_pat"} (parser >> term_pat)
+  end
+  *}
+  
+setup {* term_pat_setup *}
+
+ML {*
+  fun rtac rl = resolve0_tac [rl]
+
+  fun atac i = PRIMSEQ (Thm.assumption NONE i);
+*}
+
+ML {*
+  fun gen_all maxidx0 th =
+  let
+    val thy = Thm.theory_of_thm th;
+    val maxidx = Thm.maxidx_thm th maxidx0;
+    val prop = Thm.prop_of th;
+    fun elim (x, T) =
+      Thm.forall_elim (Thm.global_cterm_of thy (Var ((x, maxidx + 1), T)));
+  in fold elim (Drule.outer_params prop) th end;
+*}
+
+end
