26b2a2eb9eec9c059574d7b857cabbb9915b4e7e
Matthew Di Meglio <mdimeglio@optusnet.com.au>
2018-08-21 13:29:48 +1000

    Added all DataLayout work so far and it compiles.
    This includes:
    * Core data structures
    * Surface to core transformation
    * QuickCheck generators and properties
    
    TODO:
    * Figure out how to integrate QuickCheck tests into testing framework
    * Remove QuickCheck build-depends and QuickCheck exposed-modules from
      library in cogent.cabal (because it belongs somewhere else)
    * Find actual SourcePos and delete Cogent.DataLayout.Syntax module
    * Remove Cogent.Common.Repr in favour of Cogent.DataLayout.Surface
    * Rename structures in Cogent.Common.Repr to DataLayout.

---
+++ cogent/src/Cogent/DataLayout/QuickCheck/Surface.hs
@@ -0,0 +1,73 @@
+module Cogent.DataLayout.QuickCheck.Surface where
+  
+import Data.Set (Set, union, empty, intersection)
+import qualified Data.Set as S
+
+import Data.Map (Map)
+import qualified Data.Map as M
+
+import Control.Monad (guard)
+
+import Test.QuickCheck
+
+import Cogent.Common.Syntax (FieldName, TagName)
+import Cogent.DataLayout.Syntax (SourcePos)
+import Cogent.DataLayout.Surface
+import Cogent.DataLayout.QuickCheck.TypeCheck (bitSizeToRepSize)
+
+
+instance Arbitrary RepSize where
+  arbitrary = sized $
+    \totalSize -> return $ bitSizeToRepSize $ fromIntegral totalSize
+
+instance Arbitrary RepExpr where
+  arbitrary = sized genRepExpr
+
+genRepExpr :: Int -> Gen RepExpr
+genRepExpr size = oneof
+  [ genPrim size
+  , genRecord size
+  , genOffset size
+  , genVariant size
+  ]
+  where
+    genPrim :: Int -> Gen RepExpr
+    genPrim size = Prim <$> arbitrary
+    
+    genRecord :: Int -> Gen RepExpr
+    genRecord size = Record <$> genFields size
+      
+    genFields :: Int -> Gen [(FieldName, SourcePos, RepExpr)]
+    genFields size = do
+      fieldSize <- choose (0, size)
+      if fieldSize == 0
+      then return []
+      else do
+        otherFields <- genFields (size - fieldSize)
+        fieldName <- arbitrary
+        fieldRepExpr <- genRepExpr fieldSize
+        return $ (fieldName, (), fieldRepExpr) : otherFields
+        
+    
+    genVariant :: Int -> Gen RepExpr
+    genVariant size = do
+      tagSize <- choose (0, size)
+      tagExpr <- genPrim tagSize
+      alternatives <- genAlternatives (size - tagSize)
+      return $ Variant tagExpr alternatives
+      
+    
+    genAlternatives :: Int -> Gen [(TagName, SourcePos, Integer, RepExpr)]
+    genAlternatives size = do
+      altSize <- choose (0, size)
+      if altSize == 0
+      then return []
+      else do
+        otherAlts <- genAlternatives (size - altSize)
+        altName <- arbitrary
+        altValue <- arbitrary
+        altRepExpr <- genRepExpr altSize
+        return $ (altName, (), altValue, altRepExpr) : otherAlts
+    
+    genOffset :: Int -> Gen RepExpr
+    genOffset size = Offset <$> (genRepExpr size) <*> arbitrary
