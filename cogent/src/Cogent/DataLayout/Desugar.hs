5fce46d2ff7d058d2d5428aee0c6c93a3bfd536a
Matthew Di Meglio <mdimeglio@optusnet.com.au>
2018-08-29 18:09:45 +1000

    Setup for implementing separated typechecking and desugaring.
    Not sure why `make test-tc` has `fail_wf-take-nonexisting-field.cogent` broken.

--- cogent/src/Cogent/DataLayout/Desugar.hs
+++ cogent/src/Cogent/DataLayout/Desugar.hs
@@ -1,21 +1,22 @@
 module Cogent.DataLayout.Desugar where
 import Data.Map (Map)
 
-import Cogent.Compiler (__fixme)
-import Cogent.Common.Syntax (RepName)
-import Cogent.DataLayout.Surface (RepExpr)
+import Cogent.Compiler 				(__fixme, __impossible)
+import Cogent.Common.Syntax 		(DataLayoutName)
+import Cogent.Common.Types 			(Sigil(Unboxed, Boxed))
+import Cogent.DataLayout.Surface 	(DataLayoutExpr)
 import Cogent.DataLayout.Core
-
-
-type DataLayoutName = RepName -- TODO: eliminate RepName
-type DataLayoutExpr = RepExpr
-
-
-type DataLayoutDefs = Map DataLayoutName (DataLayout BitRange) 
 
 -- TODO: Split dataLayoutSurfaceToCore in Cogent.DataLayout.Typecheck
 -- into a type checking function and this desugar function!
 
-desugarDataLayout :: DataLayoutDefs -> DataLayoutExpr -> DataLayout BitRange
-desugarDataLayout _ _ = __fixme $ UnitLayout
+desugarDataLayout :: DataLayoutExpr -> DataLayout BitRange
+desugarDataLayout _ = __fixme $ UnitLayout
 
+
+-- Type checking, and the post type checking normalisation (Cogent.TypeCheck.Post)
+-- guarantees that Boxed types have an associated layout
+desugarSigil :: Sigil (Maybe DataLayoutExpr) -> Sigil (DataLayout BitRange)
+desugarSigil Unboxed              = Unboxed
+desugarSigil (Boxed ro (Just l))  = Boxed ro (desugarDataLayout l)
+desugarSigil (Boxed ro Nothing)   = __impossible $ "desugarSigil (Nothing is not in WHNF)"
