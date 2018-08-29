edbd4af3c85aa90055722ddf3909a713ed991a9f
Matthew Di Meglio <mdimeglio@optusnet.com.au>
2018-08-30 08:53:44 +1000

    Fixed regression where desugarSigil was being called on sigils with Nothing as their layout.
    Turns out this is allowed because boxed abstract types will have no associated layout and
    won't be managed by Dargent.

--- cogent/src/Cogent/Core.hs
+++ cogent/src/Cogent/Core.hs
@@ -65,13 +65,13 @@ import qualified Text.PrettyPrint.ANSI.Leijen as L ((<$>))
 data Type t
   = TVar (Fin t)
   | TVarBang (Fin t)
-  | TCon TypeName [Type t] (Sigil (DataLayout BitRange))
+  | TCon TypeName [Type t] (Sigil (Maybe (DataLayout BitRange))) -- Layout will be nothing for abstract types
   | TFun (Type t) (Type t)
   | TPrim PrimInt
   | TString
   | TSum [(TagName, (Type t, Bool))]  -- True means taken (since 2.0.4)
   | TProduct (Type t) (Type t)
-  | TRecord [(FieldName, (Type t, Bool))] (Sigil (DataLayout BitRange))  -- True means taken
+  | TRecord [(FieldName, (Type t, Bool))] (Sigil (Maybe (DataLayout BitRange)))  -- True means taken, Layout will be nothing for abstract types
   | TUnit
   | TArray (Type t) ArraySize  -- use Int for now
                                -- XXX | ^^^ (UntypedExpr t 'Zero VarName)  -- stick to UntypedExpr to be simple / zilinc
