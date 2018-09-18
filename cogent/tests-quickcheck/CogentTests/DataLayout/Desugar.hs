883b3a388cd0f3e9768fe47f7785dabf3b49a9f4
Matthew Di Meglio <mdimeglio@optusnet.com.au>
2018-09-18 13:52:26 +1000

    Added some tests for the desugaring generation of layouts for types.

--- cogent/tests-quickcheck/CogentTests/DataLayout/Desugar.hs
+++ cogent/tests-quickcheck/CogentTests/DataLayout/Desugar.hs
@@ -5,14 +5,24 @@ import Test.QuickCheck
 import Test.QuickCheck.All
 import CogentTests.DataLayout.Core
 import CogentTests.DataLayout.TypeCheck (undesugarDataLayout)
+import CogentTests.Core (genLayoutableType)
+import Cogent.DataLayout.CoreTypeCheck (matchesDataLayout, checkDataLayout)
 import Cogent.DataLayout.Desugar
-
 
 prop_returnTrip :: Property
 prop_returnTrip =
   forAll (genDataLayout size) $ \(layout, _) ->
     desugarDataLayout (undesugarDataLayout layout) == layout
   where size = 30
+
+prop_constructDataLayoutMatchingLayout =
+  forAll (sized genLayoutableType) $ \coreType ->
+    matchesDataLayout coreType (constructDataLayout coreType)
+
+prop_constructDataLayoutProducesValidLayout = 
+  forAll (sized genLayoutableType) $ \coreType ->
+    checkDataLayout (constructDataLayout coreType)
+
 
 return []
 testAll = $quickCheckAll
