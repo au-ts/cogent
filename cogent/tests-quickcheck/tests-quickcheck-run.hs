fc208191175fe3b9cb2082a0f34b54f3603d6d5f
Matthew Di Meglio <mdimeglio@optusnet.com.au>
2018-10-01 21:44:19 +1000

    Started Dargent codegen
    * Implemented generation of getters and setters for AlignedBitRanges
    * Added sanity check function which will print out generated getters and setters for some examples,
      into the log for the quickcheck tests. (No auto testing is done here because haven't set up
      exporting generated C and compiling and receiving results from within Haskell)

--- cogent/tests-quickcheck/tests-quickcheck-run.hs
+++ cogent/tests-quickcheck/tests-quickcheck-run.hs
@@ -6,8 +6,10 @@ import System.Exit
 import CogentTests.DataLayout.TypeCheck
 import CogentTests.DataLayout.Core
 import CogentTests.DataLayout.Desugar
+import CogentTests.DataLayout.CodeGen
 
 main = do
+  compileSanityCheck
   isSuccess <- foldr <$> pure (&&) <*> pure True <*> sequenceA
       [ CogentTests.DataLayout.TypeCheck.testAll
       , CogentTests.DataLayout.Core.testAll
