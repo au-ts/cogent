1ddb63d77e51a6f3a412f97a84df2b1ba86f461d
vjackson725 <vjackson725@users.noreply.github.com>
2018-10-31 17:09:14 +1100

    proof: remove kinding envs from subtyping

--- cogent/isa/Cogent.thy
+++ cogent/isa/Cogent.thy
@@ -613,78 +613,78 @@ lemma eval_prim_op_lit_type:
 
 section {* Typing rules *}
 
-inductive type_lub :: "kind env \<Rightarrow> type \<Rightarrow> type \<Rightarrow> type \<Rightarrow> bool" ("_ \<turnstile> _ \<leftarrow> _ \<squnion> _" [60,0,0,60] 60)
-  and type_glb :: "kind env \<Rightarrow> type \<Rightarrow> type \<Rightarrow> type \<Rightarrow> bool" ("_ \<turnstile> _ \<leftarrow> _ \<sqinter> _" [60,0,0,60] 60)
+inductive type_lub :: "type \<Rightarrow> type \<Rightarrow> type \<Rightarrow> bool" ("_ \<leftarrow> _ \<squnion> _" [60,0,60] 60)
+  and type_glb :: "type \<Rightarrow> type \<Rightarrow> type \<Rightarrow> bool" ("_ \<leftarrow> _ \<sqinter> _" [60,0,60] 60)
   where
   lub_tvar   : "\<lbrakk> n = n1
                 ; n2 = n1
-                \<rbrakk> \<Longrightarrow> K \<turnstile> TVar n \<leftarrow> TVar n1 \<squnion> TVar n2"
+                \<rbrakk> \<Longrightarrow> TVar n \<leftarrow> TVar n1 \<squnion> TVar n2"
 | lub_tvarb  : "\<lbrakk> n = n1
                 ; n2 = n1
-                \<rbrakk> \<Longrightarrow> K \<turnstile> TVarBang n \<leftarrow> TVarBang n1 \<squnion> TVarBang n2"
+                \<rbrakk> \<Longrightarrow> TVarBang n \<leftarrow> TVarBang n1 \<squnion> TVarBang n2"
 | lub_tcon   : "\<lbrakk> n = n1 ; n2 = n1
                 ; s = s1 ; s2 = s1
-                ; list_all3 (type_lub K) ts ts1 ts2
-                \<rbrakk> \<Longrightarrow> K \<turnstile> TCon n ts s \<leftarrow> TCon n1 ts1 s1 \<squnion> TCon n2 ts2 s2"
-| lub_tfun   : "\<lbrakk> K \<turnstile> t \<leftarrow> t1 \<sqinter> t2
-                ; K \<turnstile> u \<leftarrow> u1 \<squnion> u2
-                \<rbrakk> \<Longrightarrow> K \<turnstile> TFun t u \<leftarrow> TFun t1 u1 \<squnion> TFun t2 u2"
+                ; list_all3 type_lub ts ts1 ts2
+                \<rbrakk> \<Longrightarrow> TCon n ts s \<leftarrow> TCon n1 ts1 s1 \<squnion> TCon n2 ts2 s2"
+| lub_tfun   : "\<lbrakk> t \<leftarrow> t1 \<sqinter> t2
+                ; u \<leftarrow> u1 \<squnion> u2
+                \<rbrakk> \<Longrightarrow> TFun t u \<leftarrow> TFun t1 u1 \<squnion> TFun t2 u2"
 | lub_tprim  : "\<lbrakk> p = p1
                 ; p2 = p1
-                \<rbrakk> \<Longrightarrow> K \<turnstile> TPrim p \<leftarrow> TPrim p1 \<squnion> TPrim p2"
-| lub_trecord: "\<lbrakk> \<And>n t b t1 t2 b1 b2. \<lbrakk> (n,t,b) \<in> set ts; (n,t1,b1) \<in> set ts1; (n,t2,b2) \<in> set ts2 \<rbrakk> \<Longrightarrow> K \<turnstile> t \<leftarrow> t1 \<squnion> t2 \<and> (b = inf b1 b2)
+                \<rbrakk> \<Longrightarrow> TPrim p \<leftarrow> TPrim p1 \<squnion> TPrim p2"
+| lub_trecord: "\<lbrakk> \<And>n t b t1 t2 b1 b2. \<lbrakk> (n,t,b) \<in> set ts; (n,t1,b1) \<in> set ts1; (n,t2,b2) \<in> set ts2 \<rbrakk> \<Longrightarrow> t \<leftarrow> t1 \<squnion> t2 \<and> (b = inf b1 b2)
                 ; distinct (map fst ts)
                 ; fst ` set ts = fst ` set ts1
                 ; fst ` set ts2 = fst ` set ts1
                 ; s = s1
                 ; s1 = s2
-                \<rbrakk> \<Longrightarrow> K \<turnstile> TRecord ts s \<leftarrow> TRecord ts1 s1 \<squnion> TRecord ts2 s2"
-| lub_tprod  : "\<lbrakk> K \<turnstile> t \<leftarrow> t1 \<squnion> t2
-                ; K \<turnstile> u \<leftarrow> u1 \<squnion> u2
-                \<rbrakk> \<Longrightarrow> K \<turnstile> TProduct t u \<leftarrow> TProduct t1 u1 \<squnion> TProduct t2 u2"
-| lub_tsum   : "\<lbrakk> \<And>n t b t1 t2 b1 b2. \<lbrakk> (n,t,b) \<in> set ts; (n,t1,b1) \<in> set ts1; (n,t2,b2) \<in> set ts2 \<rbrakk> \<Longrightarrow> K \<turnstile> t \<leftarrow> t1 \<squnion> t2 \<and> (b = inf b1 b2)
+                \<rbrakk> \<Longrightarrow> TRecord ts s \<leftarrow> TRecord ts1 s1 \<squnion> TRecord ts2 s2"
+| lub_tprod  : "\<lbrakk> t \<leftarrow> t1 \<squnion> t2
+                ; u \<leftarrow> u1 \<squnion> u2
+                \<rbrakk> \<Longrightarrow> TProduct t u \<leftarrow> TProduct t1 u1 \<squnion> TProduct t2 u2"
+| lub_tsum   : "\<lbrakk> \<And>n t b t1 t2 b1 b2. \<lbrakk> (n,t,b) \<in> set ts; (n,t1,b1) \<in> set ts1; (n,t2,b2) \<in> set ts2 \<rbrakk> \<Longrightarrow> t \<leftarrow> t1 \<squnion> t2 \<and> (b = inf b1 b2)
                 ; distinct (map fst ts)
                 ; fst ` set ts = fst ` set ts1
                 ; fst ` set ts2 = fst ` set ts1
-                \<rbrakk> \<Longrightarrow> K \<turnstile> TSum ts \<leftarrow> TSum ts1 \<squnion> TSum ts2"
-| lub_tunit  : "K \<turnstile> TUnit \<leftarrow> TUnit \<squnion> TUnit"
+                \<rbrakk> \<Longrightarrow> TSum ts \<leftarrow> TSum ts1 \<squnion> TSum ts2"
+| lub_tunit  : "TUnit \<leftarrow> TUnit \<squnion> TUnit"
 
 | glb_tvar   : "\<lbrakk> n = n1
                 ; n2 = n1
-                \<rbrakk> \<Longrightarrow> K \<turnstile> TVar n \<leftarrow> TVar n1 \<sqinter> TVar n2"
+                \<rbrakk> \<Longrightarrow> TVar n \<leftarrow> TVar n1 \<sqinter> TVar n2"
 | glb_tvarb  : "\<lbrakk> n = n1
                 ; n2 = n1
-                \<rbrakk> \<Longrightarrow> K \<turnstile> TVarBang n \<leftarrow> TVarBang n1 \<sqinter> TVarBang n2"
+                \<rbrakk> \<Longrightarrow> TVarBang n \<leftarrow> TVarBang n1 \<sqinter> TVarBang n2"
 | glb_tcon   : "\<lbrakk> n = n1 ; n2 = n1
                 ; s = s1 ; s2 = s1
-                ; list_all3 (type_glb K) ts ts1 ts2
-                \<rbrakk> \<Longrightarrow> K \<turnstile> TCon n ts s \<leftarrow> TCon n1 ts1 s1 \<sqinter> TCon n2 ts2 s2"
-| glb_tfun   : "\<lbrakk> K \<turnstile> t \<leftarrow> t1 \<squnion> t2
-                ; K \<turnstile> u \<leftarrow> u1 \<sqinter> u2
-                \<rbrakk> \<Longrightarrow> K \<turnstile> TFun t u \<leftarrow> TFun t1 u1 \<sqinter> TFun t2 u2"
+                ; list_all3 type_glb ts ts1 ts2
+                \<rbrakk> \<Longrightarrow> TCon n ts s \<leftarrow> TCon n1 ts1 s1 \<sqinter> TCon n2 ts2 s2"
+| glb_tfun   : "\<lbrakk> t \<leftarrow> t1 \<squnion> t2
+                ; u \<leftarrow> u1 \<sqinter> u2
+                \<rbrakk> \<Longrightarrow> TFun t u \<leftarrow> TFun t1 u1 \<sqinter> TFun t2 u2"
 | glb_tprim  : "\<lbrakk> p = p1
                 ; p2 = p1
-                \<rbrakk> \<Longrightarrow> K \<turnstile> TPrim p \<leftarrow> TPrim p1 \<sqinter> TPrim p2"
-| glb_trecord: "\<lbrakk> \<And>n t b t1 t2 b1 b2. \<lbrakk> (n,t,b) \<in> set ts; (n,t1,b1) \<in> set ts1; (n,t2,b2) \<in> set ts2 \<rbrakk> \<Longrightarrow> K \<turnstile> t \<leftarrow> t1 \<sqinter> t2 \<and> (b = sup b1 b2)
+                \<rbrakk> \<Longrightarrow> TPrim p \<leftarrow> TPrim p1 \<sqinter> TPrim p2"
+| glb_trecord: "\<lbrakk> \<And>n t b t1 t2 b1 b2. \<lbrakk> (n,t,b) \<in> set ts; (n,t1,b1) \<in> set ts1; (n,t2,b2) \<in> set ts2 \<rbrakk> \<Longrightarrow> t \<leftarrow> t1 \<sqinter> t2 \<and> (b = sup b1 b2)
                 ; distinct (map fst ts)
                 ; fst ` set ts = fst ` set ts1
                 ; fst ` set ts2 = fst ` set ts1
                 ; s = s1
                 ; s1 = s2
-                \<rbrakk> \<Longrightarrow> K \<turnstile> TRecord ts s \<leftarrow> TRecord ts1 s1 \<sqinter> TRecord ts2 s2"
-| glb_tprod  : "\<lbrakk> K \<turnstile> t \<leftarrow> t1 \<sqinter> t2
-                ; K \<turnstile> u \<leftarrow> u1 \<sqinter> u2
-                \<rbrakk> \<Longrightarrow> K \<turnstile> TProduct t u \<leftarrow> TProduct t1 u1 \<sqinter> TProduct t2 u2"
-| glb_tsum   : "\<lbrakk> \<And>n t b t1 t2 b1 b2. \<lbrakk> (n,t,b) \<in> set ts; (n,t1,b1) \<in> set ts1; (n,t2,b2) \<in> set ts2 \<rbrakk> \<Longrightarrow> K \<turnstile> t \<leftarrow> t1 \<sqinter> t2 \<and> (b = sup b1 b2)
+                \<rbrakk> \<Longrightarrow> TRecord ts s \<leftarrow> TRecord ts1 s1 \<sqinter> TRecord ts2 s2"
+| glb_tprod  : "\<lbrakk> t \<leftarrow> t1 \<sqinter> t2
+                ; u \<leftarrow> u1 \<sqinter> u2
+                \<rbrakk> \<Longrightarrow> TProduct t u \<leftarrow> TProduct t1 u1 \<sqinter> TProduct t2 u2"
+| glb_tsum   : "\<lbrakk> \<And>n t b t1 t2 b1 b2. \<lbrakk> (n,t,b) \<in> set ts; (n,t1,b1) \<in> set ts1; (n,t2,b2) \<in> set ts2 \<rbrakk> \<Longrightarrow> t \<leftarrow> t1 \<sqinter> t2 \<and> (b = sup b1 b2)
                 ; distinct (map fst ts)
                 ; fst ` set ts = fst ` set ts1
                 ; fst ` set ts2 = fst ` set ts1
-                \<rbrakk> \<Longrightarrow> K \<turnstile> TSum ts \<leftarrow> TSum ts1 \<sqinter> TSum ts2"
-| glb_tunit  : "K \<turnstile> TUnit \<leftarrow> TUnit \<sqinter> TUnit"
+                \<rbrakk> \<Longrightarrow> TSum ts \<leftarrow> TSum ts1 \<sqinter> TSum ts2"
+| glb_tunit  : "TUnit \<leftarrow> TUnit \<sqinter> TUnit"
 
 
-definition subtyping :: "kind env \<Rightarrow> type \<Rightarrow> type \<Rightarrow> bool" ("_ \<turnstile> _ \<sqsubseteq> _" [30,0,0] 60) where
-  "(K \<turnstile> t1 \<sqsubseteq> t2) \<equiv> (K \<turnstile> t1 \<leftarrow> t1 \<squnion> t2)"
+definition subtyping :: "type \<Rightarrow> type \<Rightarrow> bool" ("_ \<sqsubseteq> _" [30,30] 60) where
+  "(t1 \<sqsubseteq> t2) \<equiv> (t1 \<leftarrow> t1 \<squnion> t2)"
 
 
 inductive typing :: "('f \<Rightarrow> poly_type) \<Rightarrow> kind env \<Rightarrow> ctx \<Rightarrow> 'f expr \<Rightarrow> type \<Rightarrow> bool"
@@ -1231,14 +1231,14 @@ lemma type_lub_type_glb_wellformed:
     "K \<turnstile> t1 wellformed"
     "K \<turnstile> t2 wellformed"
   shows
-    "K \<turnstile> t \<leftarrow> t1 \<squnion> t2 \<Longrightarrow> K \<turnstile> t wellformed"
-    "K \<turnstile> t \<leftarrow> t1 \<sqinter> t2 \<Longrightarrow> K \<turnstile> t wellformed"
+    "t \<leftarrow> t1 \<squnion> t2 \<Longrightarrow> K \<turnstile> t wellformed"
+    "t \<leftarrow> t1 \<sqinter> t2 \<Longrightarrow> K \<turnstile> t wellformed"
   using assms
 proof (induct rule: type_lub_type_glb.inducts)
   case lub_tcon then show ?case
     by (fastforce simp add: list_all3_conv_all_nth Ball_def in_set_conv_nth)
 next
-  case (lub_trecord ts ts1 ts2 K s s1 s2)
+  case (lub_trecord ts ts1 ts2 s s1 s2)
   { fix n t b
     assume n_in_ts: "(n,t,b) \<in> set ts"
     obtain t1 b1 where "(n,t1,b1) \<in> set ts1"
@@ -1253,7 +1253,7 @@ next
   } then show ?case
     using lub_trecord by auto
 next
-  case (lub_tsum ts ts1 ts2 K)
+  case (lub_tsum ts ts1 ts2)
   { fix n t b
     assume n_in_ts: "(n,t,b) \<in> set ts"
     obtain t1 b1 where "(n,t1,b1) \<in> set ts1"
@@ -1271,7 +1271,7 @@ next
   case glb_tcon then show ?case
     by (fastforce simp add: list_all3_conv_all_nth Ball_def in_set_conv_nth)
 next
-  case (glb_trecord ts ts1 ts2 K s s1 s2)
+  case (glb_trecord ts ts1 ts2 s s1 s2)
   { fix n t b
     assume n_in_ts: "(n,t,b) \<in> set ts"
     obtain t1 b1 where "(n,t1,b1) \<in> set ts1"
@@ -1286,7 +1286,7 @@ next
   } then show ?case
     using glb_trecord by auto
 next
-  case (glb_tsum ts ts1 ts2 K)
+  case (glb_tsum ts ts1 ts2)
   { fix n t b
     assume n_in_ts: "(n,t,b) \<in> set ts"
     obtain t1 b1 where "(n,t1,b1) \<in> set ts1"
@@ -1305,15 +1305,15 @@ qed auto
 lemma type_lub_type_glb_idem:
   assumes "K \<turnstile> t wellformed"
   shows
-  "K \<turnstile> t \<leftarrow> t \<squnion> t"
-  "K \<turnstile> t \<leftarrow> t \<sqinter> t"
+    "t \<leftarrow> t \<squnion> t"
+    "t \<leftarrow> t \<sqinter> t"
   using assms
 proof (induct t)
   case (TCon ns ts s)
   moreover assume "K \<turnstile> TCon ns ts s wellformed"
   ultimately show
-    "K \<turnstile> TCon ns ts s \<leftarrow> TCon ns ts s \<squnion> TCon ns ts s"
-    "K \<turnstile> TCon ns ts s \<leftarrow> TCon ns ts s \<sqinter> TCon ns ts s"
+    "TCon ns ts s \<leftarrow> TCon ns ts s \<squnion> TCon ns ts s"
+    "TCon ns ts s \<leftarrow> TCon ns ts s \<sqinter> TCon ns ts s"
      by (simp, fastforce simp add: list_all3_same kinding_all_set intro!: type_lub_type_glb.intros)+
 next
   case (TSum ts)
@@ -1330,14 +1330,14 @@ next
       by fastforce+
     ultimately have
       "b1 = b" "b2 = b"
-      "K \<turnstile> t \<leftarrow> t1 \<squnion> t2 \<and> (b = inf b1 b2)"
-      "K \<turnstile> t \<leftarrow> t1 \<sqinter> t2 \<and> (b = sup b1 b2)"
+      "t \<leftarrow> t1 \<squnion> t2 \<and> (b = inf b1 b2)"
+      "t \<leftarrow> t1 \<sqinter> t2 \<and> (b = sup b1 b2)"
       using TSum ts_wellformed kinding_variant_all_wellformed
       by auto
   }
   then show
-    "K \<turnstile> TSum ts \<leftarrow> TSum ts \<squnion> TSum ts"
-    "K \<turnstile> TSum ts \<leftarrow> TSum ts \<sqinter> TSum ts"
+    "TSum ts \<leftarrow> TSum ts \<squnion> TSum ts"
+    "TSum ts \<leftarrow> TSum ts \<sqinter> TSum ts"
     using ts_wellformed
     by (auto intro!: type_lub_type_glb.intros)
 next
@@ -1355,14 +1355,14 @@ next
       by fastforce+
     ultimately have
       "b1 = b" "b2 = b"
-      "K \<turnstile> t \<leftarrow> t1 \<squnion> t2 \<and> (b = inf b1 b2)"
-      "K \<turnstile> t \<leftarrow> t1 \<sqinter> t2 \<and> (b = sup b1 b2)"
+      "t \<leftarrow> t1 \<squnion> t2 \<and> (b = inf b1 b2)"
+      "t \<leftarrow> t1 \<sqinter> t2 \<and> (b = sup b1 b2)"
       using TRecord ts_wellformed
       by auto
   }
   then show
-    "K \<turnstile> TRecord ts s \<leftarrow> TRecord ts s \<squnion> TRecord ts s"
-    "K \<turnstile> TRecord ts s \<leftarrow> TRecord ts s \<sqinter> TRecord ts s"
+    "TRecord ts s \<leftarrow> TRecord ts s \<squnion> TRecord ts s"
+    "TRecord ts s \<leftarrow> TRecord ts s \<sqinter> TRecord ts s"
     using ts_wellformed
     by (auto intro!: type_lub_type_glb.intros)
 qed (force intro: type_lub_type_glb.intros)+
@@ -1370,16 +1370,16 @@ qed (force intro: type_lub_type_glb.intros)+
 lemma type_lub_type_glb_commut:
   assumes "K \<turnstile> t wellformed"
   shows
-  "K \<turnstile> t \<leftarrow> t1 \<squnion> t2 \<Longrightarrow> K \<turnstile> t \<leftarrow> t2 \<squnion> t1"
-  "K \<turnstile> t \<leftarrow> t1 \<sqinter> t2 \<Longrightarrow> K \<turnstile> t \<leftarrow> t2 \<sqinter> t1"
+  "t \<leftarrow> t1 \<squnion> t2 \<Longrightarrow> t \<leftarrow> t2 \<squnion> t1"
+  "t \<leftarrow> t1 \<sqinter> t2 \<Longrightarrow> t \<leftarrow> t2 \<sqinter> t1"
   using assms
 proof (induct rule: type_lub_type_glb.inducts)
-  case (lub_tcon ns ns1 ns2 s s2 s1 K ts ts1 ts2)
+  case (lub_tcon ns ns1 ns2 s s2 s1 ts ts1 ts2)
   then show ?case
     by (fastforce simp add: list_all3_conv_all_nth kinding_all_set
         intro!: type_lub_type_glb.intros)
 next
-  case (lub_trecord ts ts1 ts2 K s s2 s1)
+  case (lub_trecord ts ts1 ts2 s s2 s1)
   then show ?case
   proof (intro type_lub_type_glb.intros)
     fix n t t1 t2 b b1 b2
@@ -1387,12 +1387,12 @@ next
       "(n, t, b) \<in> set ts"
       "(n, t1, b1) \<in> set ts1"
       "(n, t2, b2) \<in> set ts2"
-    then show "K \<turnstile> t \<leftarrow> t2 \<squnion> t1 \<and> b = inf b2 b1"
+    then show "t \<leftarrow> t2 \<squnion> t1 \<and> b = inf b2 b1"
       using lub_trecord
       by (auto simp add: inf_commute)
   qed simp+
 next
-  case (lub_tsum ts ts1 ts2 K)
+  case (lub_tsum ts ts1 ts2)
   then show ?case
   proof (intro type_lub_type_glb.intros)
     fix n t t1 t2 b b1 b2
@@ -1400,7 +1400,7 @@ next
       "(n, t, b) \<in> set ts"
       "(n, t1, b1) \<in> set ts1"
       "(n, t2, b2) \<in> set ts2"
-    then show "K \<turnstile> t \<leftarrow> t2 \<squnion> t1 \<and> b = inf b2 b1"
+    then show "t \<leftarrow> t2 \<squnion> t1 \<and> b = inf b2 b1"
       using lub_tsum
       by (auto simp add: inf_commute)
   qed simp+
@@ -1410,7 +1410,7 @@ next
     using kinding_typelist_wellformed_elem
     by (auto intro!: type_lub_type_glb.intros simp add: list_all3_conv_all_nth)
 next
-  case (glb_trecord ts ts1 ts2 K s s2 s1)
+  case (glb_trecord ts ts1 ts2 s s2 s1)
   then show ?case
   proof (intro type_lub_type_glb.intros)
     fix n t t1 t2 b b1 b2
@@ -1418,12 +1418,12 @@ next
       "(n, t, b) \<in> set ts"
       "(n, t1, b1) \<in> set ts1"
       "(n, t2, b2) \<in> set ts2"
-    then show "K \<turnstile> t \<leftarrow> t2 \<sqinter> t1 \<and> b = sup b2 b1"
+    then show "t \<leftarrow> t2 \<sqinter> t1 \<and> b = sup b2 b1"
       using glb_trecord
       by (auto simp add: sup_commute)
   qed simp+
 next
-  case (glb_tsum ts ts1 ts2 K)
+  case (glb_tsum ts ts1 ts2)
   then show ?case
   proof (intro type_lub_type_glb.intros)
     fix n t t1 t2 b b1 b2
@@ -1431,7 +1431,7 @@ next
       "(n, t, b) \<in> set ts"
       "(n, t1, b1) \<in> set ts1"
       "(n, t2, b2) \<in> set ts2"
-    then show "K \<turnstile> t \<leftarrow> t2 \<sqinter> t1 \<and> b = sup b2 b1"
+    then show "t \<leftarrow> t2 \<sqinter> t1 \<and> b = sup b2 b1"
       using glb_tsum
       by (auto simp add: sup_commute)
   qed simp+
