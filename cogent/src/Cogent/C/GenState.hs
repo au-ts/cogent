0797278fac80031686b48e80f941ad6219e03f4f
Matthew Di Meglio <mdimeglio@optusnet.com.au>
2018-11-18 18:42:17 +1100

    Reduced repetition in DataLayout.CodeGen, implemented unit, variant getter/setter.
    All test-cg and test-gcc tests now pass.
    All pass_datalayout_... test_aq tests pass.

--- cogent/src/Cogent/C/GenState.hs
+++ cogent/src/Cogent/C/GenState.hs
@@ -423,7 +423,7 @@ isTrivialCDesignator (CDesignE e) = isTrivialCExpr e
 isTrivialCDesignator (CDesignFld _) = True
 
 -- *****************************************************************************
--- ** Naming convensions
+-- ** Naming conventions
 
 -- NOTE: Reserved names; users should NOT use them in Cogent programs!
 --       Prefixing underscores are disliked by C-parser / zilinc
