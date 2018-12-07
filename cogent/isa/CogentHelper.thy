b529071fdc5136a158e4f5bc92835cf53145645e
vjackson725 <vjackson725@users.noreply.github.com>
2018-12-07 15:22:42 +1100

    proof: relicense the core proofs under BSD

--- cogent/isa/CogentHelper.thy
+++ cogent/isa/CogentHelper.thy
@@ -4,10 +4,10 @@
  * ABN 41 687 119 230.
  *
  * This software may be distributed and modified according to the terms of
- * the GNU General Public License version 2. Note that NO WARRANTY is provided.
- * See "LICENSE_GPLv2.txt" for details.
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
  *
- * @TAG(DATA61_GPL)
+ * @TAG(DATA61_BSD)
  *)
 
 theory CogentHelper
