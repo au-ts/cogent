35d1c987171d476bf58a648c8faece8b749a2869
vjackson725 <vjackson725@users.noreply.github.com>
2018-12-07 15:27:48 +1100

    proof: relicense the c-refinement proofs under BSD

--- c-refinement/Cogent_Corres_Shallow_C.thy
+++ c-refinement/Cogent_Corres_Shallow_C.thy
@@ -1,11 +1,13 @@
 (*
- * Copyright 2016, NICTA
+ * Copyright 2018, Data61
+ * Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+ * ABN 41 687 119 230.
  *
  * This software may be distributed and modified according to the terms of
- * the GNU General Public License version 2. Note that NO WARRANTY is provided.
- * See "LICENSE_GPLv2.txt" for details.
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
  *
- * @TAG(NICTA_GPL)
+ * @TAG(DATA61_BSD)
  *)
 
 (* Refinement chain from C to shallow-embedded Cogent. *)
