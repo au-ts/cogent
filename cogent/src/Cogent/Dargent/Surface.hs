2281ee839682ecb7b0b18bc3d683ac61a42a3505
Zilin Chen <Zilin.Chen@data61.csiro.au>
2019-03-04 13:43:22 +1100

    compiler: add license header for Dargent files
    [skip ci][skip lemma]

--- cogent/src/Cogent/Dargent/Surface.hs
+++ cogent/src/Cogent/Dargent/Surface.hs
@@ -1,4 +1,17 @@
+--
+-- Copyright 2018, Data61
+-- Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+-- ABN 41 687 119 230.
+--
+-- This software may be distributed and modified according to the terms of
+-- the GNU General Public License version 2. Note that NO WARRANTY is provided.
+-- See "LICENSE_GPLv2.txt" for details.
+--
+-- @TAG(DATA61_GPL)
+--
+
 module Cogent.Dargent.Surface where
+
 import Cogent.Common.Syntax (FieldName, TagName, RepName, Size)
 import Text.Parsec.Pos (SourcePos)
 import Cogent.Compiler (__fixme)
