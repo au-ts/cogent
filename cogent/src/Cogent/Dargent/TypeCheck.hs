8b95cc608b0fc615d61767e436c6a8a8951cdd60
Zilin Chen <Zilin.Chen@data61.csiro.au>
2019-03-04 13:43:22 +1100

    compiler: add license header for Dargent files
    [skip ci][skip lemma]

--- cogent/src/Cogent/Dargent/TypeCheck.hs
+++ cogent/src/Cogent/Dargent/TypeCheck.hs
@@ -1,8 +1,21 @@
+--
+-- Copyright 2018, Data61
+-- Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+-- ABN 41 687 119 230.
+--
+-- This software may be distributed and modified according to the terms of
+-- the GNU General Public License version 2. Note that NO WARRANTY is provided.
+-- See "LICENSE_GPLv2.txt" for details.
+--
+-- @TAG(DATA61_GPL)
+--
+
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE TupleSections #-}
+
 module Cogent.Dargent.TypeCheck where
 
 import Data.Map (Map)
