9968fd0395bd30aacf745ea5ce4b178ed6ce318f
Vivian Dang <vivian@cog.systems>
2019-05-22 23:12:35 +1000

    WIP lexer

--- cogent/src/Cogent/PrettifyLexer.hs
+++ cogent/src/Cogent/PrettifyLexer.hs
@@ -15,7 +15,7 @@
 -- let success error branch
 -- Quantifier
 
-import Data.Char(isSpace, isAlpha)
+import Data.Char(isSpace, isAlpha, isDigit)
 
 data Token
     = Kwd Keyword
@@ -27,6 +27,7 @@ data Token
     | Dot | Ddot | Underscore | 
     | Unbox | Typeapp | 
     | Llikely | Likely | MLikely
+    | Number Int
     deriving(Show)
 
 data Keyword 
@@ -68,8 +69,34 @@ lexer ('=':'>':cs) = MLikely : lexer cs
 lexer ('~':'>':cs) = LLikely : lexer cs
 lexer (c:cs) | isAlpha c = let
     (word, rest) = span isAlpha (c:cs)
-    in checkKeyword word : lexer rest
+    in toToken word : lexer rest
     where
-      -- check keyword stuff
+        toToken :: String -> Token
+        toToken "let" = Kwd Let
+        toToken "in" = Kwd In
+        toToken "type" = Kwd Type
+        toToken "include" = Kwd Include
+        toToken "all" = Kwd All
+        toToken "take" = Kwd Take
+        toToken "put" = Kwd Put
+        toToken "inline" = Kwd Inline
+        toToken "upcast" = Kwd Upcast
+        toToken "repr" = Kwd Repr
+        toToken "variant" = Kwd Variant
+        toToken "record" = Kwd Record
+        toToken "at" = Kwd At
+        toToken "if" = Kwd If
+        toToken "then" = Kwd Then
+        toToken "else" = Kwd Else
+        toToken "not" = Kwd Not
+        toToken "complement" = Kwd Complement
+        toToken "and" = Kwd And
+        toToken "true" = Kwd True
+        toToken "false" = Kwd False
+        toToken "o" = Kwd O  -- what is "o"?
+
+lexer (c:cs) | isDigit c = let
+    (numStr, rest) = span isDigit(c:cs)
+    in Number (read numStr): lexer rest
 
 lexer _ = []
