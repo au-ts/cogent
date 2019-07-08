934d0291f1af0a0f9e6ff39f8e377ebaa2461bc3
vjackson725 <vjackson725@users.noreply.github.com>
2019-07-08 11:02:05 +1000

    dargent: pretty print layouts

--- cogent/src/Cogent/TypeCheck/Base.hs
+++ cogent/src/Cogent/TypeCheck/Base.hs
@@ -95,6 +95,7 @@ data TypeError = FunctionNotFound VarName
                | CustTyGenIsSynonym TCType
                | TypeWarningAsError TypeWarning
                | DataLayoutError DataLayoutTypeCheckError
+               | LayoutOnNonRecordOrCon TCType
                deriving (Eq, Show, Ord)
 
 isWarnAsError :: TypeError -> Bool
