69c52aa4abd09dcb17d60e6e26ff2cf5d0c8ca13
Zilin Chen <Zilin.Chen@data61.csiro.au>
2019-08-21 14:48:18 +1000

    compiler: fix test-quickcheck
    broken due to dargent changes
    
    [skip lemma]

--- cogent/tests-quickcheck/CogentTests/Dargent/TypeCheck.hs
+++ cogent/tests-quickcheck/CogentTests/Dargent/TypeCheck.hs
@@ -1,7 +1,19 @@
+--
+-- Copyright 2018, Data61
+-- Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+-- ABN 41 687 119 230.
+--
+-- This software may be distributed and modified according to the terms of
+-- the GNU General Public License version 2. Note that NO WARRANTY is provided.
+-- See "LICENSE_GPLv2.txt" for details.
+--
+-- @TAG(DATA61_GPL)
+--
+
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TemplateHaskell #-}
-module CogentTests.DataLayout.TypeCheck where
+module CogentTests.Dargent.TypeCheck where
 
 import Data.Set (Set, union, empty, intersection)
 import qualified Data.Set as S
@@ -15,11 +27,11 @@ import Test.QuickCheck
 import Test.QuickCheck.All
 import Text.Parsec.Pos (SourcePos)
 
-import Cogent.DataLayout.Surface
-import Cogent.DataLayout.Core
-import Cogent.DataLayout.TypeCheck
-import CogentTests.DataLayout.Core
-import Cogent.Common.Syntax (RepName, Size)
+import Cogent.Dargent.Surface
+import Cogent.Dargent.Core
+import Cogent.Dargent.TypeCheck
+import CogentTests.Dargent.Core
+import Cogent.Common.Syntax (DataLayoutName, Size)
 
 {- PROPERTIES -}
 
@@ -43,11 +55,11 @@ prop_typeCheckValidGivesNoErrors =
 
 {-+ INVERSE FUNCTIONS
   |
-  | Convert core DataLayout values back to surface RepExprs for round trip testing.
+  | Convert core DataLayout values back to surface DataLayoutExprs for round trip testing.
   +-}
   
-bitSizeToRepSize :: Size -> RepSize
-bitSizeToRepSize size =
+bitSizeToDataLayoutSize :: Size -> DataLayoutSize
+bitSizeToDataLayoutSize size =
   if bytes == 0
     then Bits bits
   else if bits == 0
@@ -59,14 +71,14 @@ bitSizeToRepSize size =
     
 undesugarBitRange :: BitRange -> DataLayoutExpr
 undesugarBitRange (BitRange size offset) =
-  Offset (Prim (bitSizeToRepSize size)) (bitSizeToRepSize offset)
+  Offset (Prim (bitSizeToDataLayoutSize size)) (bitSizeToDataLayoutSize offset)
     
 undesugarDataLayout  :: DataLayout BitRange -> DataLayoutExpr
-undesugarDataLayout  UnitLayout = Prim (Bits 0)
-undesugarDataLayout  (PrimLayout bitRange) = undesugarBitRange bitRange
-undesugarDataLayout  (RecordLayout fields) =
+undesugarDataLayout UnitLayout = Prim (Bits 0)
+undesugarDataLayout (PrimLayout bitRange) = undesugarBitRange bitRange
+undesugarDataLayout (RecordLayout fields) =
   Record $ fmap (\(name, (layout, pos)) -> (name, pos, (undesugarDataLayout  layout))) (M.toList fields)
-undesugarDataLayout  (SumLayout tagBitRange alternatives) =
+undesugarDataLayout (SumLayout tagBitRange alternatives) =
   Variant
     (undesugarBitRange tagBitRange)
     (fmap (\(tagName, (tagValue, altLayout, altPos)) -> (tagName, altPos, tagValue, (undesugarDataLayout  altLayout))) (M.toList alternatives))
