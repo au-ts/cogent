d47e71f4d95ca382b4796a5173e46bacdad17059
Zilin Chen <Zilin.Chen@data61.csiro.au>
2019-08-21 14:48:18 +1000

    compiler: fix test-quickcheck
    broken due to dargent changes
    
    [skip lemma]

---
+++ cogent/tests-quickcheck/CogentTests/Dargent/Desugar.hs
@@ -0,0 +1,40 @@
+--
+-- Copyright 2018, Data61
+-- Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+-- ABN 41 687 119 230.
+--
+-- This software may be distributed and modified according to the terms of
+-- the GNU General Public License version 2. Note that NO WARRANTY is provided.
+-- See "LICENSE_GPLv2.txt" for details.
+--
+-- @TAG(DATA61_GPL)
+--
+
+{-# LANGUAGE TemplateHaskell #-}
+module CogentTests.Dargent.Desugar where
+
+import Test.QuickCheck
+import Test.QuickCheck.All
+import CogentTests.Dargent.Core
+import CogentTests.Dargent.TypeCheck (undesugarDataLayout)
+import CogentTests.Core (genLayoutableType)
+import Cogent.Dargent.Common (DargentLayout(..))
+import Cogent.Dargent.CoreTypeCheck (matchesDataLayout, checkDataLayout)
+import Cogent.Dargent.Desugar
+
+prop_returnTrip :: Property
+prop_returnTrip =
+  forAll (genDataLayout size) $ \(layout, _) ->
+    desugarDataLayout (undesugarDataLayout layout) == layout
+  where size = 30
+
+prop_constructDataLayoutMatchingLayout =
+  forAll (sized genLayoutableType) $ \coreType ->
+    matchesDataLayout coreType (constructDataLayout coreType)
+
+prop_constructDataLayoutProducesValidLayout = 
+  forAll (sized genLayoutableType) $ \coreType ->
+    checkDataLayout (Layout $ constructDataLayout coreType)
+
+return []
+testAll = $quickCheckAll
--- cogent/tests-quickcheck/tests-quickcheck-run.hs
+++ cogent/tests-quickcheck/tests-quickcheck-run.hs
@@ -1,19 +1,33 @@
+--
+-- Copyright 2018, Data61
+-- Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+-- ABN 41 687 119 230.
+--
+-- This software may be distributed and modified according to the terms of
+-- the GNU General Public License version 2. Note that NO WARRANTY is provided.
+-- See "LICENSE_GPLv2.txt" for details.
+--
+-- @TAG(DATA61_GPL)
+--
+
 {-# LANGUAGE TemplateHaskell #-}
+
 module Main where
+
 import Test.QuickCheck
 import Control.Monad
 import System.Exit
-import CogentTests.DataLayout.TypeCheck
-import CogentTests.DataLayout.Core
-import CogentTests.DataLayout.Desugar
-import CogentTests.DataLayout.CodeGen
+import CogentTests.Dargent.TypeCheck
+import CogentTests.Dargent.Core
+import CogentTests.Dargent.Desugar
+import CogentTests.Dargent.CodeGen
 
 main = do
   compileSanityCheck
   isSuccess <- foldr <$> pure (&&) <*> pure True <*> sequenceA
-      [ CogentTests.DataLayout.TypeCheck.testAll
-      , CogentTests.DataLayout.Core.testAll
-      , CogentTests.DataLayout.Desugar.testAll
+      [ CogentTests.Dargent.TypeCheck.testAll
+      , CogentTests.Dargent.Core.testAll
+      , CogentTests.Dargent.Desugar.testAll
       ]
   if isSuccess
     then exitSuccess
