9422d02af67ca562d86df69dd217e2dfe6e6b37a
Zilin Chen <Zilin.Chen@data61.csiro.au>
2019-12-09 16:00:03 +1100

    compiler: bundle all the Isabelle modules
    [skip lemma]

---
+++ cogent/src/Cogent/Isabelle.hs
@@ -0,0 +1,41 @@
+--
+-- Copyright 2019, Data61
+-- Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+-- ABN 41 687 119 230.
+--
+-- This software may be distributed and modified according to the terms of
+-- the GNU General Public License version 2. Note that NO WARRANTY is provided.
+-- See "LICENSE_GPLv2.txt" for details.
+--
+-- @TAG(DATA61_GPL)
+--
+
+module Cogent.Isabelle (
+  -- * Re-export all the top-level functions in Cogent.Isabelle.*
+  module Cogent.Isabelle.ACInstall
+, module Cogent.Isabelle.AllRefine
+, module Cogent.Isabelle.CorresProof
+, module Cogent.Isabelle.CorresSetup
+, module Cogent.Isabelle.Deep
+, module Cogent.Isabelle.MonoProof
+, module Cogent.Isabelle.NormalProof
+, module Cogent.Isabelle.Root
+, module Cogent.Isabelle.Shallow
+, module Cogent.Isabelle.ShallowTable
+, module Cogent.Isabelle.TypeProofs
+, module Cogent.Isabelle.GraphGen
+) where
+
+import Cogent.Isabelle.ACInstall    (acInstallDefault)
+import Cogent.Isabelle.AllRefine    (allRefine)
+import Cogent.Isabelle.CorresProof  (corresProof)
+import Cogent.Isabelle.CorresSetup  (corresSetup)
+import Cogent.Isabelle.Deep         (deep)
+import Cogent.Isabelle.MonoProof    (monoProof)
+import Cogent.Isabelle.NormalProof  (normalProof)
+import Cogent.Isabelle.Root         (root)
+import Cogent.Isabelle.Shallow      (shallowConsts, shallow, shallowTuplesProof)
+import Cogent.Isabelle.ShallowTable (st, printTable)
+import Cogent.Isabelle.TypeProofs   (deepTypeProof)
+import Cogent.Isabelle.GraphGen     (graphGen)
+
