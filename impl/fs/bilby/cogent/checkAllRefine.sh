1f633693aae4221fc49c0ba7a62ae8e833394905
Bilby Regression <bilby_regression@ertos.nicta.com.au>
2019-12-17 10:33:21 +1100

    compiler: fix isabelle naming issue, update bilbyfs defns and tests, add script to check allRefine

---
+++ impl/fs/bilby/cogent/checkAllRefine.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# Runs AllRefine on the generated bilbyfs proofs
+#
+
+if [[ ! -f "plat/verification/BilbyFs_AllRefine.thy" ]]
+then
+    echo "Error - Cannot find BilbyFs_AllRefine.thy"
+    exit
+fi
+
+export L4V_ARCH="ARM";
+
+# This gives java more heap space to use, so it won't crash as often
+export ISABELLE_BUILD_JAVA_OPTIONS="-Xms2048m -Xmx12288m -Xss4m";
+
+export REPO_ROOT="../../../.."
+
+time isabelle build -D plat/verification \
+                    -d $REPO_ROOT/cogent/isa \
+                    -d $REPO_ROOT/autocorres \
+                    -b -o process_output_limit=999 \
+                     BilbyFs_AllRefine;
