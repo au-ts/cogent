1ed8313889567f73fe9bf024fa45b482d36c2fd3
Jashank Jeremy <jashank.jeremy@unsw.edu.au>
2020-01-07 14:26:05 +1100

    docs: Ignore the `_build' directory when building.
    [ci skip]

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,8 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build']
+
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = None
