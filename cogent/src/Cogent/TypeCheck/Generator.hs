24193bdde70fb7ce8ddd409b776680ae6c241e96
Zilin Chen <Zilin.Chen@data61.csiro.au>
2020-01-15 18:49:37 +1100

    compiler.array: [wip] it's looping somewhere...

--- cogent/src/Cogent/TypeCheck/Generator.hs
+++ cogent/src/Cogent/TypeCheck/Generator.hs
@@ -85,8 +85,9 @@ validateType (RT t) = do
   ts <- lift $ use knownTypes
   layouts <- lift $ use knownDataLayouts
   case t of
-    TVar v _ | v `notElem` vs -> freshTVar >>= \t' -> return (Unsat $ UnknownTypeVariable v, t')
-    TCon t as _ | Nothing <- lookup t ts -> freshTVar >>= \t' -> return (Unsat $ UnknownTypeConstructor t, t')
+    TVar v b | v `notElem` vs -> freshTVar >>= \t' -> return (Unsat $ UnknownTypeVariable v, t')
+             | otherwise -> return (mempty, T $ TVar v b)
+    TCon t as s | Nothing <- lookup t ts -> freshTVar >>= \t' -> return (Unsat $ UnknownTypeConstructor t, t')
                 | Just (vs', _) <- lookup t ts
                 , provided <- length as
                 , required <- length vs'
@@ -94,6 +95,7 @@ validateType (RT t) = do
                -> freshTVar >>= \t' -> return (Unsat $ TypeArgumentMismatch t provided required, t')
                 | Just (vs, Just x) <- lookup t ts
                -> second (Synonym t) <$> fmapFoldM validateType as
+                | otherwise -> (second T) <$> fmapFoldM validateType (TCon t as s)
     TRecord fs s | fields  <- map fst fs
                  , fields' <- nub fields
                 -> let toRow (T (TRecord fs s)) = R (Row.fromList fs) (Left s)
@@ -101,9 +103,13 @@ validateType (RT t) = do
                         then case s of
                           Boxed _ (Just dlexpr)
                             | Left (anError : _) <- runExcept $ tcDataLayoutExpr layouts dlexpr  -- layout is bad
-                            -> freshTVar >>= \t' -> return (Unsat $ DataLayoutError anError, t')
-                          _ -> __todo "validateType"  -- layout is good, or no layout
+                            -> freshTVar >>= \t' -> return (Unsat $ DataLayoutError anError, t') 
+                          _ -> -- layout is good, or no layout
+                               -- We have to pattern match on 'TRecord' otherwise it's a type error.
+                               do (c, TRecord fs' s') <- fmapFoldM validateType t
+                                  return (c, toRow. T $ TRecord fs' s')
                         else freshTVar >>= \t' -> return (Unsat $ DuplicateRecordFields (fields \\ fields'), t')
+                  | otherwise -> (second T) <$> fmapFoldM validateType (TRecord fs s)
     TVariant fs  -> do let tuplize [] = T TUnit
                            tuplize [x] = x
                            tuplize xs  = T (TTuple xs)
@@ -119,6 +125,8 @@ validateType (RT t) = do
       let cl = Arith (SE (T bool) (PrimOp ">" [x, SE (T u32) (IntLit 0) ?loc]) ?loc)
       (c,te') <- validateType te
       return (c <> ctkn <> cl, A te' x (Left s) mhole)
+    -- TATake es t -> undefined
+    -- TAPut  es t -> undefined
 #endif
     TLayout l t -> do
       let cl = case runExcept $ tcDataLayoutExpr layouts l of
@@ -126,7 +134,14 @@ validateType (RT t) = do
                  Right _    -> Sat
       (ct,t') <- validateType t
       pure (cl <> ct, T $ TLayout l t')
-    _ -> __todo "validateType" -- fmapFoldM validateType t
+    -- vvv The uninteresting cases; but we still have to match each of them to convince the typechecker / zilinc
+    TFun t1 t2 -> (second T) <$> fmapFoldM validateType (TFun t1 t2)
+    TTuple ts  -> (second T) <$> fmapFoldM validateType (TTuple ts)
+    TUnit -> return (mempty, T TUnit)
+    TUnbox t  -> (second T) <$> fmapFoldM validateType (TUnbox t)
+    TBang  t  -> (second T) <$> fmapFoldM validateType (TBang  t)
+    TTake mfs t -> (second T) <$> fmapFoldM validateType (TTake mfs t)
+    TPut  mfs t -> (second T) <$> fmapFoldM validateType (TPut  mfs t)
     -- With (TCon _ _ l), and (TRecord _ l), must check l == Nothing iff it is contained in a TUnbox.
     -- This can't be done in the current setup because validateType' has no context for the type it is validating.
     -- Not implementing this now, because a new syntax for types is needed anyway, which may make this issue redundant.
