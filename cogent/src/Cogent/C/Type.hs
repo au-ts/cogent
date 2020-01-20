0656afc851a0fecc45630ba31276f6fced0cb7e2
Zhenyu Yao <z5125769@student.unsw.edu.au>
2020-01-20 16:44:46 +1100

    dargent: add array layout support
    [skip lemma]

--- cogent/src/Cogent/C/Type.hs
+++ cogent/src/Cogent/C/Type.hs
@@ -114,7 +114,13 @@ genTyDecl (Function t1 t2, n) tns =
   where fty = if __cogent_funtyped_func_enum then untypedFuncEnum else unitT
 #ifdef BUILTIN_ARRAYS
 genTyDecl (Array t, n) _ = [CDecl $ CVarDecl t n True Nothing]
-genTyDecl (ArrayL _, n) _ = [genTySynDecl (n, CPtr (CInt False CCharT))]  -- always @uint8_t*@
+genTyDecl (ArrayL layout len, n) _ =
+  let elemSize = dataLayoutSizeBytes layout
+      arrSize  = CBinOp C.Mul len (CConst $ CNumConst elemSize (CInt False CIntT) DEC)
+      arrType  = CArray (CInt False CIntT) (CArraySize arrSize)
+   in if elemSize == 0
+         then []
+         else [CDecl $ CStructDecl n [(arrType, Just "data")], genTySynDecl (n, CStruct n)]
 #endif
 genTyDecl (AbsType x, n) _ = [CMacro $ "#include <abstract/" ++ x ++ ".h>"]
 
@@ -169,7 +175,7 @@ lookupTypeCId (TRecord fs (Boxed _ CLayout)) =
 lookupTypeCId cogentType@(TRecord _ (Boxed _ _)) = __impossible "lookupTypeCId: record with non-record layout"
 #ifdef BUILTIN_ARRAYS
 lookupTypeCId (TArray t n Unboxed _) = getCompose (Compose . lookupStrlTypeCId =<< Array <$> (Compose . lookupType) t)
-lookupTypeCId (TArray t n (Boxed _ l) _) = lookupStrlTypeCId (ArrayL l)
+lookupTypeCId (TArray t n (Boxed _ l) _) = genLExpr n >>= (\x -> lookupStrlTypeCId (ArrayL l x))
 #endif
 lookupTypeCId t = Just <$> typeCId t
 
@@ -229,7 +235,11 @@ typeCId t = use custTypeGen >>= \ctg ->
     typeCId' (TUnit) = return unitT
 #ifdef BUILTIN_ARRAYS
     typeCId' (TArray t l Unboxed _) = getStrlTypeCId =<< Array <$> genType t
-    typeCId' (TArray t l (Boxed _ _) _) = __todo "typeCId'"
+    typeCId' (TArray t l (Boxed _ al) _) =
+      case al of
+        Layout ArrayLayout {} -> genLExpr l >>= (\x -> getStrlTypeCId (ArrayL al x))
+        CLayout -> getStrlTypeCId =<< Array <$> genType t
+        _ -> __impossible "Tried to get the c-type of an array with a non-array record"
 #endif
 
     typeCIdFlat :: CC.Type 'Zero VarName -> Gen v CId
@@ -281,8 +291,10 @@ genType t@(TRecord _ s)  | s /= Unboxed = CPtr . CIdent <$> typeCId t
 genType t@(TString)                     = CPtr . CIdent <$> typeCId t
 genType t@(TCon _ _ s)   | s /= Unboxed = CPtr . CIdent <$> typeCId t
 #ifdef BUILTIN_ARRAYS
-genType (TArray t l s _) | s /= Unboxed = CPtr <$> genType t  -- If it's heap-allocated, we don't care about the size / zilinc
-                         | otherwise    = CArray <$> genType t <*> (CArraySize <$> genLExpr l)
+genType t@(TArray elt l s _)
+  | (Boxed _ CLayout) <- s = CPtr <$> genType elt  -- If it's heap-allocated without layout specified
+  | (Boxed _ al)      <- s = CPtr . CIdent <$> typeCId t -- we are going to declare it as a type
+  | otherwise              = CArray <$> genType elt <*> (CArraySize <$> genLExpr l)
 #endif
 genType t                               = CIdent <$> typeCId t
 
