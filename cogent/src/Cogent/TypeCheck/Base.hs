49ca03c2716cee7094742a05efb467e9125c76a0
Emmet Murray <e.minor.murray@gmail.com>
2020-02-21 11:33:46 +1100

    compiler: fix recursive types unrolling prematurely

--- cogent/src/Cogent/TypeCheck/Base.hs
+++ cogent/src/Cogent/TypeCheck/Base.hs
@@ -298,14 +298,6 @@ data FuncOrVar = MustFunc | MustVar | FuncOrVar deriving (Eq, Ord, Show)
 floppy :: TCType -> Bool
 floppy = not . rigid
 
--- returns true if a type has a (TBang | TTake | TPut) wrapping it
-wobbly :: TCType -> Bool
-wobbly (T (TBang {})) = True
-wobbly (T (TTake {})) = True
-wobbly (T (TPut {})) = True
-wobbly _ = False
-
-
 funcOrVar :: TCType -> FuncOrVar
 funcOrVar (U _) = FuncOrVar
 funcOrVar (T (TVar  {})) = FuncOrVar
