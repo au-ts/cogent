56d5025a1f9fe61fbed5525d9a8b3a0115af0a92
Zilin Chen <Zilin.Chen@data61.csiro.au>
2020-02-26 00:43:12 +1100

    compiler.array: it loops.
    [skip lemma][skip ci]

--- cogent/src/Cogent/TypeCheck/Solver/SMT.hs
+++ cogent/src/Cogent/TypeCheck/Solver/SMT.hs
@@ -33,8 +33,8 @@ import Control.Monad.Trans.RWS (RWST (..))
 import qualified Data.IntMap as IM (empty)
 import qualified Data.Map as M (Map, empty, map, toList)
 import Data.Maybe (fromMaybe)
-import Data.SBV (SatResult (..), ThmResult (..), SMTResult (..), z3)
-import Data.SBV.Dynamic (satWith, proveWith, SMTConfig (..))
+import Data.SBV (AllSatResult (..), SatResult (..), ThmResult (..), SMTResult (..), z3)
+import Data.SBV.Dynamic (allSatWith, satWith, proveWith, SMTConfig (..))
 import Lens.Micro
 import Lens.Micro.Mtl (view)
 import qualified Text.PrettyPrint.ANSI.Leijen as L
@@ -65,9 +65,11 @@ smtSolve =
     SmtState c <- get
     let ks' = constEquations ks
     traceTc "sol/smt" (L.text "Constants" L.<> L.colon L.<$> L.prettyList ks')
-    b <- liftIO $ smtSat $ implTCSExpr (andTCSExprs ks') (andTCSExprs c)
-    case b of True  -> hoistMaybe $ Just gs
-              False -> hoistMaybe $ Nothing
+    res <- liftIO $ smtSatResult $ implTCSExpr (andTCSExprs ks') (andTCSExprs c)
+    case res of (_ , _, []) -> hoistMaybe $ Nothing   -- returns no models
+                (False, False, [m]) -> hoistMaybe $ Just gs  -- TODO: only one model, we should generate assignment
+                (_, _, ms) -> hoistMaybe $ Just gs  -- TODO: multiple models. check if the unifs are
+                                                    -- the only occurrences. if so, then generate assignment.
    )
 
 -- | Converts the store of known constants to equality constraints.
@@ -90,24 +92,25 @@ extractPredicates = pickOne' $ \g -> do
     hoistMaybe $ Just [g & goal .~ c']
 
 -- | Returns a detailed result of satisfiability of a logical predicate.
-smtSatResult :: TCSExpr -> IO SMTResult
+smtSatResult :: TCSExpr -> IO (Bool, Bool, [SMTResult])
 smtSatResult e = do
   dumpMsgIfTrue __cogent_ddump_smt (warn "SMT solving:" L.<+> L.pretty e L.<> L.hardline)
   -- NOTE: sbv will perform Skolemisation to reduce existentials, while preserving satisfiability. / zilinc
-  SatResult s <- satWith (z3 { verbose = __cogent_ddump_smt
-                             , redirectVerbose = Just $ fromMaybe "/dev/stderr" __cogent_ddump_to_file })
-                         (evalStateT (sexprToSmt e)
-                         (SmtTransState IM.empty M.empty))
+  res@(AllSatResult (limit, _, unknown, models)) <-
+    allSatWith (z3 { verbose = __cogent_ddump_smt
+                   , redirectVerbose = Just $ fromMaybe "/dev/stderr" __cogent_ddump_to_file })
+               (evalStateT (sexprToSmt e)
+               (SmtTransState IM.empty M.empty))
   dumpMsgIfTrue __cogent_ddump_smt (L.text (replicate 80 '-') L.<> L.hardline)
   traceTc "sol/smt" (L.text "Running SMT on expression"
                      L.<$> indent' (L.pretty e)
                      L.<$> L.text "gives result"
-                     L.<$> indent' (L.text . show $ SatResult s))
-  return s
+                     L.<$> indent' (L.text $ show res))
+  return (limit, unknown, models)
 
 -- | Only returns 'True' or 'False'.
 smtSat :: TCSExpr -> IO Bool
 smtSat e = 
   smtSatResult e >>= \case
-    Satisfiable {} -> return True
+    (_, False, models) | length models > 0 -> return True
     _ -> return False
