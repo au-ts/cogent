aa168289e0e74b335eda834574b7483c580a4575
Zhenyu Yao <z5125769@student.unsw.edu.au>
2020-03-06 14:04:25 +1100

    compiler.dargent: multiple fixes
    [skip lemma]
    
    - add checks for substitued layouts within TLApp
    - fix parser
    - add simplify rule for TVar
    - fix core tc
    - possible TODO: add constraints for layout expressions

--- cogent/src/Cogent/Dargent/TypeCheck.hs
+++ cogent/src/Cogent/Dargent/TypeCheck.hs
@@ -112,15 +112,16 @@ toTCDL DLPtr = TLPtr
 tcDataLayoutExpr :: NamedDataLayouts -> [DLVarName] -> DataLayoutExpr -> Except [DataLayoutTcError] Allocation
 tcDataLayoutExpr env vs (DLRepRef n s) =
   case M.lookup n env of
-    Just ([], _, Just allocation) -> mapPaths (InDecl n) $ return allocation
-    Just ([], _, Nothing)         -> throwE [BadDataLayout n PathEnd]
-    Just (vars, expr, _) | length vars == length s
-                         -> tcDataLayoutExpr env vs (substDataLayoutExpr (zip vars s) expr)
-                         | length vars >  length s
-                         -> throwE [TooFewDataLayoutArgs n PathEnd]
-                         | length vars <  length s
-                         -> throwE [TooManyDataLayoutArgs n PathEnd]
-    Nothing                       -> throwE [UnknownDataLayout n PathEnd]
+    Just ([], _, Just allocation) | length s > 0 -> throwE [TooManyDataLayoutArgs n PathEnd]
+                                  | otherwise    -> mapPaths (InDecl n) $ return allocation
+    Just ([], _, Nothing) -> throwE [BadDataLayout n PathEnd]
+    Just (vars, expr, _)  | length s == length vars
+                          -> tcDataLayoutExpr env vs (substDataLayoutExpr (zip vars s) expr)
+                          | length s <  length vars
+                          -> throwE [TooFewDataLayoutArgs n PathEnd]
+                          | length s >  length vars
+                          -> throwE [TooManyDataLayoutArgs n PathEnd]
+    Nothing               -> throwE [UnknownDataLayout n PathEnd]
 
 tcDataLayoutExpr _ _ (DLPrim size) = return $ singletonAllocation (bitRange, PathEnd)
   where
@@ -182,6 +183,70 @@ tcDataLayoutExpr _ _ DLPtr = return $ singletonAllocation (pointerBitRange, Path
 tcDataLayoutExpr _ vs (DLVar n) = if n `elem` vs then return undeterminedAllocation
                                                  else throwE [UnknownDataLayoutVar n PathEnd]
 tcDataLayoutExpr _ _ l = __impossible $ "tcDataLayoutExpr; tried to typecheck unexpected layout: " ++ show l
+
+
+-- | Check the layout after subsitution within constraint generator for TLApp
+-- We may simplify this function or refactor the layout related constraints later
+tcTCDataLayout :: NamedDataLayouts -> TCDataLayout -> Except [DataLayoutTcError] Allocation
+tcTCDataLayout env (TLRepRef n s) =
+  case M.lookup n env of
+    Just ([], _, Just allocation) | length s > 0 -> throwE [TooManyDataLayoutArgs n PathEnd]
+                                  | otherwise    -> mapPaths (InDecl n) $ return allocation
+    Just ([], _, Nothing) -> throwE [BadDataLayout n PathEnd]
+    Just (vars, expr, _)  | length vars == length s
+                          -> tcTCDataLayout env (substTCDataLayout (zip vars s) $ toTCDL expr)
+                          | length vars >  length s
+                          -> throwE [TooFewDataLayoutArgs n PathEnd]
+                          | length vars <  length s
+                          -> throwE [TooManyDataLayoutArgs n PathEnd]
+    Nothing               -> throwE [UnknownDataLayout n PathEnd]
+
+tcTCDataLayout _ (TLPrim size) = return $ singletonAllocation (bitRange, PathEnd)
+  where
+    bitSize  = evalSize size
+    bitRange = fromJust $ newBitRangeBaseSize 0 bitSize {- 0 <= bitSize -}
+
+tcTCDataLayout env (TLOffset dataLayout offsetSize) =
+  offset (evalSize offsetSize) <$> tcTCDataLayout env dataLayout
+
+tcTCDataLayout env (TLRecord fields) = foldM tcField emptyAllocation fields
+  where
+    tcField accumAlloc (fn, pos, tcl) = do
+      fieldsAlloc <- mapPaths (InField fn pos) $ tcTCDataLayout env tcl
+      except $ first (fmap OverlappingBlocks) $ accumAlloc /\ fieldsAlloc
+
+tcTCDataLayout env (TLVariant tagExpr alternatives) =
+  case primitiveBitRange tagExpr of
+    Just tagBits | isZeroSizedBR tagBits -> throwE [ZeroSizedBitRange (InTag PathEnd)]
+                 | otherwise ->
+      do
+        altsAlloc <- fst <$> foldM (tcAlternative tagBits) (emptyAllocation, M.empty) alternatives
+        except $ first (fmap OverlappingBlocks) $ singletonAllocation (tagBits, InTag PathEnd) /\ altsAlloc
+    Nothing      -> throwE [TagNotSingleBlock (InTag PathEnd)]
+  where
+    tcAlternative range (accumAlloc, accumTagValues) (tagName, pos, tagValue, tcl) = do
+      alloc     <- (\/) accumAlloc <$> mapPaths (InAlt tagName pos) (tcTCDataLayout env tcl)
+      tagValues <- checkedTagValues
+      return (alloc, tagValues)
+      where
+        checkedTagValues :: Except [DataLayoutTcError] (Map Size TagName)
+        checkedTagValues
+          | tagValue < 0 || tagValue >= 2 ^ bitSizeBR range =
+            throwE [OversizedTagValue (InAlt tagName pos PathEnd) range tagName tagValue]
+          | Just conflictingTagName <- tagValue `M.lookup` accumTagValues =
+            throwE [SameTagValues (InAlt tagName pos PathEnd) conflictingTagName tagName tagValue]
+          | otherwise =
+            return $ M.insert tagValue tagName accumTagValues
+    primitiveBitRange (TLPrim size)        = newBitRangeBaseSize 0 (evalSize size)
+    primitiveBitRange (TLOffset expr size) = offset (evalSize size) <$> primitiveBitRange expr
+    primitiveBitRange _                    = Nothing
+
+#ifdef BUILTIN_ARRAYS
+tcTCDataLayout env (TLArray e p) = mapPaths (InElmt p) $ tcTCDataLayout env e
+#endif
+tcTCDataLayout _ TLPtr = return $ singletonAllocation (pointerBitRange, PathEnd)
+tcTCDataLayout _ (TLU _) = return undeterminedAllocation
+tcTCDataLayout _ l = __impossible $ "tcTCDataLayout; tried to typecheck unexpected layout: " ++ show l
 
 
 -- NOTE: the check for type-layout compatibility is in Cogent.TypeCheck.Base
