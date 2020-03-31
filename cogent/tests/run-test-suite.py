8ecddf1a23daa56f07de252b13d6c34a5a51ca99
vjackson725 <vjackson725@users.noreply.github.com>
2020-03-31 14:46:50 +1100

    tests: add type proof script

--- cogent/tests/run-test-suite.py
+++ cogent/tests/run-test-suite.py
@@ -39,11 +39,17 @@ importok = [check_import("ruamel.yaml"), check_import("termcolor")]
 if not all(importok):
     sys.exit(1)
 
+#
+# Script Starts here
+#
+
+class PreconditionViolation(Exception):
+    pass
+
 
 CONFIG_FILE_NAME = "config.yaml"
-TEST_DIST_DIR = os.path.join(os.path.dirname(
-    os.path.realpath(__file__)), "dist")
-
+TEST_SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+TEST_DIST_DIR = Path(TEST_SCRIPT_DIR, "dist")
 
 def clean_dist():
     # rm -rf
@@ -67,6 +73,28 @@ verbose_test_names = None
 #   (status, errormsg, expected)
 # where status, expected :: "pass" | "fail" | "error" | "wip"
 
+class TestContext:
+    def __init__(self, repo, cogent, dist_dir, script_dir, phases):
+        self.repo = repo
+        self.cogent = cogent
+        self.dist_dir = dist_dir
+        self.script_dir = script_dir
+        self.phases = phases
+
+class Phase:
+    def __init__(self, phase_path):
+        self.name = phase_path.stem
+        self.phase_file = phase_path.resolve()
+
+    def run(self, context, fname, test):
+        return subprocess.run([self.phase_file] + [fname],
+                                stderr = subprocess.STDOUT,
+                                stdout = subprocess.PIPE,
+                                cwd = context.script_dir,
+                                env = { "COGENT_REPO": context.repo
+                                      , "TEST_DIST_DIR": context.dist_dir
+                                      , "HOME": os.environ['HOME']
+                                      })
 
 class TestResult:
 
@@ -136,7 +164,7 @@ class InvalidConfigError(Exception):
 
 class TestConfiguration:
 
-    valid_test_fields = ["files", "flags", "expected_result", "test_name"]
+    valid_test_fields = ["files", "flags", "expected_result", "test_name", "phase"]
 
     header_block_len = 20
 
@@ -163,9 +191,6 @@ class TestConfiguration:
             if not "files" in f.keys():
                 raise InvalidConfigError(
                     "Test {0} in {1} must contain mandatory field 'files', a list with at least 1 test".format(i, self.relpath))
-            if not "flags" in f.keys():
-                raise InvalidConfigError(
-                    "Test {0} in {1} must contain mandatory field 'flags', specifying ".format(i, self.relpath))
             if not "expected_result" in f.keys():
                 raise InvalidConfigError(
                     "Test {0} in {1} must contain mandatory field 'expected_result'".format(i, self.relpath))
@@ -173,9 +198,12 @@ class TestConfiguration:
             if len(f["files"]) == 0:
                 raise InvalidConfigError(
                     "Test {0} in {1} must have at least 1 test file".format(i, self.relpath))
-            if len(f["flags"]) == 0:
-                raise InvalidConfigError(
-                    "Test {0} in {1} must have at least 1 compiler flag".format(i, self.relpath))
+            try:
+                if len(f["flags"]) == 0:
+                    raise InvalidConfigError(
+                        "Test {0} in {1} must have at least 1 compiler flag".format(i, self.relpath))
+            except KeyError:
+                pass
             if f["expected_result"] not in ["error", "pass", "fail", "wip"]:
                 raise InvalidConfigError("""Field 'expected_result' must be one of 'pass', 'fail', 'error' or 'wip' in test {0} in {1}\n. Actual value: {2}"""
                                          .format(i, self.relpath, str(f["expected_result"])))
@@ -185,10 +213,13 @@ class TestConfiguration:
                     raise InvalidConfigError(
                         "Field '{0}' not a valid field in test {1} in {2}".format(k, i, self.relpath))
 
-            for flag in f["flags"]:
-                if re.compile(r'^\s*--dist-dir').match(flag):
-                    raise InvalidConfigError(
-                        "The use of the '--dist-dir' flag is prohibited in test flags (test {}, in {})".format(i, self.relpath))
+            try:
+                for flag in f["flags"]:
+                    if re.compile(r'^\s*--dist-dir').match(flag):
+                        raise InvalidConfigError(
+                            "The use of the '--dist-dir' flag is prohibited in test flags (test {}, in {})".format(i, self.relpath))
+            except KeyError:
+                pass
 
             i += 1
 
@@ -197,7 +228,7 @@ class TestConfiguration:
 
 
     # Run the cogent compiler with the given flags, under test schema d
-    def run_cogent(self, filename, flags, test_info):
+    def run_cogent(self, context, filename, test_info):
         fname = os.path.join(self.dir, filename)
         # Check file exists and error gracefully
         if not os.path.exists(fname):
@@ -209,10 +240,46 @@ class TestConfiguration:
         def test():
             setup_dist()
 
-            res = subprocess.run(["cogent"] + flags + ["--dist-dir={}".format(TEST_DIST_DIR)] + [fname],
+            try:
+                flags = test_info['flags']
+            except KeyError:
+                flags = []
+
+            res = subprocess.run([context.cogent] + flags + ["--dist-dir={}".format(TEST_DIST_DIR)] + [fname],
                                  stderr=subprocess.STDOUT,
                                  stdout=subprocess.PIPE,
                                  cwd=self.dir)
+
+            status = "pass"
+
+            # The compiler returns an error code
+            if res.returncode == 134:
+                status = "fail"
+            # The haskell process crashes/errors
+            elif res.returncode != 0:
+                status = "error"
+
+            return (status, res.stdout.decode("utf-8"), test_info["expected_result"])
+
+        return TestResult(test(), fname, test_info['test_name'])
+
+    def run_phase(self, context, filename, phase, test_info):
+        fname = os.path.join(self.dir, filename)
+        # Check file exists and error gracefully
+        if not os.path.exists(fname):
+            def f(): return ("error", "Source file '{}' not found".format(
+                fname), test_info['expected_result'])
+            return TestResult(f(), fname, test_info['test_name'])
+
+        if not context.repo:
+            def g(): return ("error", "repo not found", test_info['expected_result'])
+            return TestResult(g(), fname, test_info['test_name'])
+
+        # function that runs our test
+        def test():
+            setup_dist()
+
+            res = phase.run(context, fname, test_info)
 
             status = "pass"
 
@@ -233,24 +300,28 @@ class TestConfiguration:
               "-" * self.header_block_len)
 
     # Run one test by name
-    def run_one(self, test_name):
-        results = []
-        for test in self.settings:
-            if test_name == test['test_name']:
-                self.print_test_header(test_name)
-                for f in test['files']:
-                    results.append(self.run_cogent(f, test['flags'], test))
-                print()
-                break
-        return results
+    def run_one(self, context, test_name):
+        return self.run_tests(context, filter(lambda t: test_name == t['test_name'], self.settings))
 
     # Run all tests in the configuration file
-    def run_all(self):
-        results = []
-        for test in self.settings:
+    def run_all(self, context):
+        return self.run_tests(context, self.settings)
+
+    def run_tests(self, context, tests):
+        for test in tests:
             self.print_test_header(test['test_name'])
             for f in test['files']:
-                results.append(self.run_cogent(f, test['flags'], test))
+                try:
+                    phasename = test['phase']
+                except KeyError:
+                    phasename = "cogent"
+                if phasename == "cogent":
+                    results.append( self.run_cogent(context, f, test) )
+                else:
+                    try:
+                        results.append( self.run_phase(context, f, context.phases[phasename], test) )
+                    except KeyError:
+                        raise PreconditionViolation("bad phase: {}".format(phasename))
             print()
         return results
 
@@ -265,17 +336,15 @@ def get_cfg_from_test_name(f):
 
 
 # Find and run one test
-def run_test(test_name):
+def run_test(context, test_name, phases):
     conf = get_cfg_from_test_name(test_name)
     if conf is None:
         print(colored("Cannot find config file containing test name {}".format(test_name), "red"))
         sys.exit(1)
-    res = conf.run_one(test_name)
+    res = conf.run_one(context, test_name)
     return res
 
 # Find all configuration files in the test directory
-
-
 def get_configs_with_errors():
     files = Path(".").rglob(CONFIG_FILE_NAME)
     files = [os.path.abspath(x) for x in files]
@@ -326,15 +395,7 @@ def validate():
         seen_names.add(n)
     return err
 
-
 if __name__ == "__main__":
-
-    # Check if cogent is installed
-    cogent = shutil.which("cogent")
-    if cogent is None:
-        print("Could not find cogent compiler on PATH - Please install cogent and place it on the PATH")
-        sys.exit(1)
-
     ap = argparse.ArgumentParser(
         description="Cogent Testing Framework",
         epilog="Test configurations must be stored in a '{}' file".format(
@@ -353,8 +414,33 @@ if __name__ == "__main__":
                     dest="validate",
                     action="store_true",
                     help="Check the format of all config files is correct")
-
+    ap.add_argument("--extra-phases", "-p",
+                    dest="phase_dir",
+                    default="phases",
+                    help="override the location of the additional phase directory")
+    ap.add_argument("--repo",
+                    dest="repo",
+                    help="test a particular repository")
     args = ap.parse_args()
+
+    if args.repo is None:
+        # Check if cogent is installed
+        cogent = shutil.which("cogent")
+        repo = None
+        if cogent is None:
+            print("Could not find cogent compiler on PATH - Please install cogent and place it on the PATH")
+            sys.exit(1)
+    else:
+        repo = Path(args.repo).resolve()
+        cogent = Path(repo, "cogent", ".cabal-sandbox", "bin", "cogent").resolve()
+        if not cogent.exists():
+            print("error: could not find the cogent compiler at '{}'".format(cogent))
+            sys.exit(1)
+
+    files = Path(args.phase_dir).glob("*.sh")
+    phases = dict(map(lambda p: (p.stem,Phase(p)), files))
+
+    context = TestContext(repo, cogent, TEST_DIST_DIR, TEST_SCRIPT_DIR, phases)
 
     # Validate all config files
     err = validate()
@@ -374,12 +460,12 @@ if __name__ == "__main__":
     results = []
     # If we're only running specific tests
     if args.only_test is not None:
-        results = run_test(args.only_test)
+        results = run_test(context, args.only_test, phases)
     # Otherwise, run all possible tests
     else:
         configs = get_configs()
         for c in configs:
-            for res in c.run_all():
+            for res in c.run_all(context):
                 results.append(res)
 
     all_passed = True
