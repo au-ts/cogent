c0716c243d75617d4d4c2d880d4ce6894bde0dc6
Lucy <lucy.qiu@unsw.edu.au>
2020-07-29 13:05:23 +1000

    minigent

--- minigent/test.js
+++ minigent/test.js
@@ -1,5 +1,5 @@
 var C = require('./minigent.js');
-var A = require('./abstract.js');
+// var A = require('./abstract.js');
 function height (r) { return C.Case((r).node, "Leaf", u=>0,node2=>C.Let((node2).val, tree=>C.Let(height((tree).left),left=>C.Let(height((tree).right),right=>((left > right) ? (left + 1) : (right + 1)))))); };
 
 
