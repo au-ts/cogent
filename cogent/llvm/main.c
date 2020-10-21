c906ff7381db92fd2bbcad737648c6482bdc5672
Harrison Scott <Harrison.Scott@data61.csiro.au>
2020-10-21 15:41:40 +1100

    compiler.llvm: add WIP kitchen sink example

--- cogent/llvm/main.c
+++ cogent/llvm/main.c
@@ -1,18 +1,6 @@
 #include <stdlib.h>
-
-typedef struct {
-  char a;
-  char b;
-} arg;
-
-extern char foo( arg * );
-
+#include "kitchen_sink.h"
 
 int main(){
-  arg * a = malloc(sizeof(arg));
-  a->a = 1;
-  a->b = 2;
-  char res = foo(a);
-  free(a);
-  return res;
+  return 0;
 }
