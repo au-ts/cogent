07530478687f5adf30e5a5f35195771f4b5d1d84
Louis Cheung <louis.cheung1@unsw.edu.au>
2020-11-02 10:54:34 +1100

    Added a more convenient corres definition for abstract functions

--- c-refinement/Cogent_Corres.thy
+++ c-refinement/Cogent_Corres.thy
@@ -1761,6 +1761,47 @@ lemma abs_rel_def':
             \<lbrace>\<lambda>y' s'. \<exists>\<sigma>' y. \<xi>' afun_name (\<sigma>, x) (\<sigma>', y) \<and> (\<sigma>',s') \<in> srel \<and> val_rel y y'\<rbrace>!)" 
   by (fastforce  simp: abs_rel_def validNF_def valid_def no_fail_def)
 
+definition
+  "abs_fun_rel \<Xi>' srel afun_name \<xi>' afun_mon \<sigma> st x x'
+    = (proc_ctx_wellformed \<Xi>' \<longrightarrow> \<xi>' matches-u \<Xi>' \<longrightarrow> (\<sigma>,st) \<in> srel \<longrightarrow>
+      (\<forall>r' w'. val_rel x x'
+        \<and> \<Xi>', \<sigma> \<turnstile> x :u fst (snd (\<Xi>' afun_name)) \<langle>r', w'\<rangle>
+        \<longrightarrow> \<not> snd (afun_mon x' st)
+            \<and> (\<forall>st' y'. (y', st') \<in> fst (afun_mon x' st)
+                \<longrightarrow> (\<exists>\<sigma>' y. \<xi>' afun_name (\<sigma>, x) (\<sigma>', y)
+                    \<and> val_rel y y' \<and> (\<sigma>', st') \<in> srel))))"
+lemma absfun_corres:
+  "abs_fun_rel \<Xi>' srel s \<xi>' afun' \<sigma> st (\<gamma> ! i) v'
+  \<Longrightarrow> i < length \<gamma> \<Longrightarrow> val_rel (\<gamma> ! i) v'
+  \<Longrightarrow> \<Gamma>' ! i = Some (fst (snd (\<Xi>' s)))
+  \<Longrightarrow> corres srel
+     (App (AFun s []) (Var i))
+     (do x \<leftarrow> afun' v'; gets (\<lambda>s. x) od)
+     \<xi>' \<gamma> \<Xi>' \<Gamma>' \<sigma> st"
+  apply (clarsimp simp: corres_def abs_fun_rel_def)
+  apply (frule matches_ptrs_length, simp)
+  apply (frule(2) matches_ptrs_proj_single')
+  apply clarsimp
+  apply (erule impE, blast)
+  apply clarsimp
+  apply (elim allE, drule mp, blast)
+  apply clarsimp
+  apply (intro exI conjI[rotated], assumption+)
+  apply (rule u_sem_abs_app)
+    apply (rule u_sem_afun)
+   apply (rule u_sem_var)
+  apply simp
+  done
+
+lemma abs_fun_rel_def':
+  "abs_fun_rel \<Xi>' srel afun_name \<xi>' afun_mon \<sigma> st x x'
+    = (proc_ctx_wellformed \<Xi>' \<longrightarrow> \<xi>' matches-u \<Xi>' \<longrightarrow> (\<sigma>,st) \<in> srel \<longrightarrow>
+        (\<forall>r' w'. val_rel x x' \<and> \<Xi>', \<sigma> \<turnstile> x :u fst (snd (\<Xi>' afun_name)) \<langle>r', w'\<rangle>
+        \<longrightarrow> \<lbrace>\<lambda>s0. s0 = st\<rbrace> 
+              afun_mon x' 
+            \<lbrace>\<lambda>y' s'. \<exists>\<sigma>' y. \<xi>' afun_name (\<sigma>, x) (\<sigma>', y) \<and> (\<sigma>',s') \<in> srel \<and> val_rel y y'\<rbrace>!))" 
+  by (fastforce  simp: abs_fun_rel_def validNF_def valid_def no_fail_def)
+
 end
 
 ML {*
