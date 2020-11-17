14acaa4e1240ef286008452f750fd8182abb1c84
Harrison Scott <Harrison.Scott@data61.csiro.au>
2020-11-17 12:17:51 +1100

    llvm.checker: hacky .ll AST importer

--- cogent/llvm/checker/coq/Checker.v
+++ cogent/llvm/checker/coq/Checker.v
@@ -1,9 +1,10 @@
-From Coq Require Import List ZArith.
+From Coq Require Import List String ZArith.
 
 From ITree Require Import ITree.
 From Vellvm Require Import LLVMAst.
 
 From Checker Require Import Cogent Compiler.
+From Input Require Import LLVM.
 
 (* Definition checkExp (cogentExp:expr) (llvmExp:exp typ) : Prop := compile cogentExp = llvmExp. *)
 
@@ -16,3 +17,7 @@ Definition prog : expr := `1 `+ `2 `* `3.
 (* Compute run prog. *)
 
 Compute (burn 100 (denote_expr prog)).
+
+(* LLVM Import *)
+
+Compute LLVMInput.
---
+++ cogent/llvm/checker/input/LLVM.template.v
@@ -0,0 +1,8 @@
+From Coq Require Import List String ZArith.
+
+From Vellvm Require Import LLVMAst.
+
+Import ListNotations.
+
+Definition LLVMInput :=
+(*
