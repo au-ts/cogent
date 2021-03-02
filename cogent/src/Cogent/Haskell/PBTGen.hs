784468778059cea6a8ea09823a32e623758c53e3
Oscar Lewis Downing <odowning19@gmail.com>
2021-03-02 12:00:18 +1100

    progress with rrel generator: unpack oc & unpack oa now working roughly

--- cogent/src/Cogent/Haskell/PBTGen.hs
+++ cogent/src/Cogent/Haskell/PBTGen.hs
@@ -123,7 +123,8 @@ propModule name hscname pbtinfos decls =
              , ImportDecl () (ModuleName () "Corres" ) False False False Nothing Nothing Nothing
              , ImportDecl () (ModuleName () hscname) False False False Nothing Nothing Nothing
              , ImportDecl () (ModuleName () "Lens.Micro") False False False Nothing Nothing Nothing
-             , ImportDecl () (ModuleName () "Lens.Micro.TH") False False False Nothing Nothing Nothing
+             , ImportDecl () (ModuleName () "Lens.Micro.TH") False False False Nothing Nothing (Just $ ImportSpecList () False (map importVar ["makeLenses"]))
+             , ImportDecl () (ModuleName () "Control.Lens.Combinators") False False False Nothing Nothing (Just $ ImportSpecList () False (map importVar ["makePrisms"]))
              -- , ImportDecl () (ModuleName () (hscname ++ "_Abs")) False False False Nothing (Just (ModuleName () "FFI")) Nothing
              , ImportDecl () (ModuleName () "Prelude"  ) False False False Nothing Nothing (Just $ ImportSpecList () False import_prelude)
              , ImportDecl () (ModuleName () "Data.Bits") False False False Nothing Nothing (Just $ ImportSpecList () False import_bits)
@@ -216,7 +217,7 @@ genDecls' PBTInfo{..} defs = do
         let FunAbsF{absf=_, ityps=ityps} = fabsf
             icT' = fromJust $ P.lookup "IC" ityps
             icT = fromJust $ P.lookup "IA" ityps
-        -- (icT, _, absE) <- genAbsFTypsAndExp fname iaT defs
+        -- (icT, _, absE) <- mkAbsFExp fname iaT defs
         let fnName = "gen_" ++fname
             --toName = "Gen " ++ show icT' -- ++icT
             --to     = TyCon   () (mkQName toName)
@@ -235,14 +236,12 @@ absFDecl PBTInfo{..} defs = do
             -- icT = fromJust $ P.lookup "IC" ityps
             iaT = fromJust $ P.lookup "IA" ityps
             fnName = "abs_" ++fname
-        (icT, _, absE, conNames) <- genAbsFTypsAndExp fname iaT defs
+        (icT, _, absE, conNames) <- mkAbsFExp fname iaT defs
         let ti     = icT
             to     = iaT
             sig    = TypeSig () [mkName fnName] (TyFun () ti to)
             dec    = FunBind () [Match () (mkName fnName) [pvar $ mkName "ic"] (UnGuardedRhs () absE) Nothing]
-            mkLens t = SpliceDecl () $ app (function "makeLenses") 
-                                (TypQuote () (UnQual () (mkName t)))
-        return $ (map mkLens conNames)++[sig, dec]
+        return $ (map mkLens (takeWhile (\x -> notElem x hsSumTypes) conNames))++[sig, dec]
 
 -- Refinement Relation Generator
 rrelDecl :: PBTInfo -> [CC.Definition TypedExpr VarName b] -> SG [Decl ()]
@@ -250,12 +249,20 @@ rrelDecl PBTInfo{..} defs = do
         let FunRRel{rrel=_, otyps=otyps} = frrel
             oaT = fromJust $ P.lookup "OA" otyps
             fnName = "rel_" ++fname
-        (ocT, _, rrelE) <- getFnOutTyp fname oaT defs
+        (ocT, _, rrelE, conNames) <- mkRrelExp fname oaT defs
+        -- (ocT, _, rrelE) <- mkAbsFExp fname oaT defs
         let to     = mkTyConT $ mkName "Bool"
             ti     = TyFun () oaT $ TyFun () ocT to
             sig    = TypeSig () [mkName fnName] (ti)
             dec    = FunBind () [Match () (mkName fnName) [pvar $ mkName "oa", pvar $ mkName "oc"] (UnGuardedRhs () rrelE) Nothing]
-        return $ [sig, dec]
+        return $ (map mkLens (takeWhile (\x -> notElem x hsSumTypes) conNames))++[sig, dec]
+
+mkLens :: String -> Decl ()
+mkLens t 
+    = let fname = if | P.head t == 'R' -> "makeLenses"
+                     | P.head t == 'V' -> "makePrisms"
+                     | otherwise -> "undefined"
+       in SpliceDecl () $ app (function fname) (TypQuote () (UnQual () (mkName t)))
  
 -- Get Function Type and Expression
 -- ------------------------------------------
@@ -263,28 +270,56 @@ rrelDecl PBTInfo{..} defs = do
 -- @iaTyp@ is the abstract input type
 -- @defs@ is the list of Cogent definitions
 --
-genAbsFTypsAndExp :: String -> Type () -> [CC.Definition TypedExpr VarName b] -> SG (Type (), Type (), Exp (), [String]) 
-genAbsFTypsAndExp fname iaTyp defs = do 
+-- ------------------------------------------------------
+mkAbsFExp :: String -> Type () -> [CC.Definition TypedExpr VarName b] -> SG (Type (), Type (), Exp (), [String]) 
+mkAbsFExp fname iaTyp defs = do 
     let def = fromJust $ find (\x -> CC.getDefinitionId x == fname) defs
-    (icT, iaT, absE, conNames) <- genAbsFTypsAndExp' def iaTyp
+    (icT, iaT, absE, conNames) <- mkAbsFExp' def iaTyp
     pure $ (icT, iaT, absE, conNames)
 
 -- Helper: get function type and expression
-genAbsFTypsAndExp' :: (CC.Definition TypedExpr VarName b) -> Type () -> SG ( (Type (), Type (), Exp (), [String]) )
-genAbsFTypsAndExp' def iaT | (CC.FunDef _ fn ps _ ti to _) <- def 
+mkAbsFExp' :: (CC.Definition TypedExpr VarName b) -> Type () -> SG ( (Type (), Type (), Exp (), [String]) )
+mkAbsFExp' def iaT | (CC.FunDef _ fn ps _ ti to _) <- def 
     = local (typarUpd (map fst $ Vec.cvtToList ps)) $ do
         ti' <- shallowType ti
         (absE, conNames) <- mkAbsFBody ti ti' iaT
-        pure $ ({-TyCon () (mkQName "Unknown") -} ti', iaT, absE, conNames)
+        pure $ (ti', iaT, absE, conNames)
 
-genAbsFTypsAndExp' def iaT | (CC.AbsDecl _ fn ps _ ti to) <- def = local (typarUpd (map fst $ Vec.cvtToList ps)) $ do
+mkAbsFExp' def iaT | (CC.AbsDecl _ fn ps _ ti to) <- def = local (typarUpd (map fst $ Vec.cvtToList ps)) $ do
     ti' <- shallowType ti
     (absE, conNames) <- mkAbsFBody ti ti' iaT
-    pure $ ( {-TyCon () (mkQName "Unknown")-} ti', iaT, absE, conNames)
+    pure $ ( ti', iaT, absE, conNames)
 
-genAbsFTypsAndExp' def iaT | (CC.TypeDef tn _ _) <- def 
+mkAbsFExp' def iaT | (CC.TypeDef tn _ _) <- def 
     = pure $ (TyCon () (mkQName "Unknown"), iaT, function $ "undefined", [])
+-- ------------------------------------------------------
 
+-- ------------------------------------------------------
+mkRrelExp :: String -> Type () -> [CC.Definition TypedExpr VarName b] -> SG (Type (), Type (), Exp (), [String]) 
+mkRrelExp fname oaTyp defs = do 
+    let def = fromJust $ find (\x -> CC.getDefinitionId x == fname) defs
+    (ocT, oaT, rrelE, conNames) <- mkRrelExp' def oaTyp
+    pure $ (ocT, oaT, rrelE, conNames)
+
+-- Helper: get function type and expression
+mkRrelExp' :: (CC.Definition TypedExpr VarName b) -> Type () -> SG ( (Type (), Type (), Exp (), [String]) )
+mkRrelExp' def oaT | (CC.FunDef _ fn ps _ ti to _) <- def 
+    = local (typarUpd (map fst $ Vec.cvtToList ps)) $ do
+        to' <- shallowType to
+        (rrel, conNames) <- mkRrelBody to to' oaT
+        pure $ (to', oaT, rrel, conNames)
+
+mkRrelExp' def oaT | (CC.AbsDecl _ fn ps _ ti to) <- def = local (typarUpd (map fst $ Vec.cvtToList ps)) $ do
+    to' <- shallowType to
+    (absE, conNames) <- mkRrelBody to to' oaT
+    pure $ ( to', oaT, absE, conNames)
+
+mkRrelExp' def oaT | (CC.TypeDef tn _ _) <- def 
+    = pure $ (TyCon () (mkQName "Unknown"), oaT, function $ "undefined", [])
+
+-- ------------------------------------------------------
+
+{-
 getFnOutTyp :: String -> Type () -> [CC.Definition TypedExpr VarName b] -> SG (Type (), Type (), Exp ()) 
 getFnOutTyp fname oaTyp defs = do 
     let d = fromJust $ find defFilt defs
@@ -308,6 +343,7 @@ getFnOutTyp' (CC.AbsDecl _ fn ps _ ti to) oaT = local (typarUpd typar) $ do
     where
         typar = map fst $ Vec.cvtToList ps
 getFnOutTyp' (CC.TypeDef tn _ _) oaT = pure $ (TyCon () (mkQName "Unknown"), oaT, function $ "undefined")
+-}
 
 -- TODO: clean up
 -- ---------------------------------
@@ -335,11 +371,26 @@ getFnOutTyp' (CC.TypeDef tn _ _) oaT = pure $ (TyCon () (mkQName "Unknown"), oaT
 -- ----------------------------------------------------------------------------------------------------
 mkAbsFBody :: CC.Type t a -> Type () -> Type () -> SG (Exp (), [String])
 mkAbsFBody cogIcTyp icTyp iaTyp 
-    = let tyL = analyseTypes cogIcTyp icTyp 0 "None"
-          lens = mkLensView tyL Nothing
+    = let icLayout = determineUnpack cogIcTyp icTyp 0 "None"
+          lens = mkLensView icLayout "ic" Nothing 
           binds = P.zip (map (\x -> pvar . mkName . fst $ x) lens) (map snd lens)
           body = packAbsCon iaTyp (map fst lens) 0
-       in pure $ (mkLetE binds body, getConNames tyL []) 
+       in pure $ (mkLetE binds body, getConNames icLayout []) 
+
+-- | mkRrelBody 
+-- | dummy func ATM
+-- --------------------------------------------------
+mkRrelBody :: CC.Type t a -> Type () -> Type () -> SG (Exp (), [String])
+mkRrelBody cogOcTyp ocTyp oaTyp
+    = let ocLy = determineUnpack cogOcTyp ocTyp 0 "None"
+          oaLy = determineUnpack' oaTyp 0 "None"
+          ocLens = mkLensView ocLy "oc" Nothing 
+          oaLens = mkLensView oaLy "oa" Nothing 
+          ls = oaLens ++ ocLens 
+          cNames = (getConNames ocLy []) ++ (getConNames oaLy [])
+          binds = P.zip (map (\x -> pvar . mkName . fst $ x) ls) (map snd ls)
+          body = function "undefined" -- packAbsCon iaTyp (map fst ls) 0
+       in pure $ (mkLetE binds body, cNames) 
 
 -- generate exp for packing the constructor that is returned by absf
 -- 1st = type
@@ -396,45 +447,45 @@ packAbsCon iaTyp varsToPut prev | _ <- iaTyp = __impossible $ "Bad Abstraction"+
 -- key = var name ++ depth tag
 -- val = 
 -- ----------------------------------------------------------------------------------------------------
-mkLensView :: HsEmbedLayout -> Maybe (Exp ()) -> [(String, (Exp ()))]
-mkLensView tyL prev 
+mkLensView :: HsEmbedLayout -> String -> Maybe (Exp ()) -> [(String, (Exp ()))]
+mkLensView tyL varToView prev
     = let hsTy = tyL ^. hsTyp
-          ty = tyL ^. grTag
+          group = tyL ^. grTag
           fld = tyL ^. fieldMap
           -- field map is a tree with int leaves -> only build vars for leaves
-       in concat $ map ( \(k, v) -> case ty of 
+       in concat $ map ( \(k, v) -> case group of 
            HsPrim -> case v of
               (Left depth) -> [ ( k ++ replicate depth (P.head "'")
                                 , case prev of Just x -> x
-                                               Nothing -> mkVar "ic" 
+                                               Nothing -> mkVar varToView
                                 )
                               ]
               (Right next) -> __impossible $ show k ++ " " ++ show v
            _ -> case v of
               (Left depth) -> __impossible $ show k ++ " " ++ show v
-              (Right next) -> mkLensView next $ Just $ mkViewInfixE "ic" ty prev k
+              (Right next) -> mkLensView next varToView $ Just $ mkViewInfixE varToView group prev k
        ) (M.toList fld) 
 
--- | analyseTypes
+-- | determineUnpack
 -- ---------------
 -- | Builds the structure used for generating the body of absf
 -- ----------------------------------------------------------------------------------------------------
-analyseTypes :: CC.Type t a -> Type () -> Int -> String -> HsEmbedLayout
-analyseTypes cogIcTyp (TyParen _ t   ) depth fieldName = analyseTypes cogIcTyp t depth fieldName
-analyseTypes cogIcTyp icTyp depth fieldName 
+determineUnpack :: CC.Type t a -> Type () -> Int -> String -> HsEmbedLayout
+determineUnpack cogIcTyp (TyParen _ t   ) depth fieldName = determineUnpack cogIcTyp t depth fieldName
+determineUnpack cogIcTyp icTyp depth fieldName 
     | (TyTuple _ _ tfs) <- icTyp = HsEmbedLayout icTyp HsTuple $ getTupFields cogIcTyp tfs
     where getTupFields cogTy tfs 
             = let fs = getFields cogTy
                 in M.fromList [ let k = "_"++show (i+1)
-                                  in (k , Right (analyseTypes (fst (snd (fs!!i))) (tfs!!i) (depth+1) k))
+                                  in (k , Right (determineUnpack (fst (snd (fs!!i))) (tfs!!i) (depth+1) k))
                               | i <- [0..(P.length tfs)-1]
                               ]
-analyseTypes cogIcTyp icTyp depth fieldName 
+determineUnpack cogIcTyp icTyp depth fieldName 
     | (TyCon _ cn) <- icTyp = HsEmbedLayout icTyp HsPrim (getConFields cn)
     where getConFields cn = M.fromList $ case (checkIsPrim cn) of 
                           Just x -> [(fieldName, Left depth)] 
                           Nothing -> []
-analyseTypes cogIcTyp icTyp depth fieldName 
+determineUnpack cogIcTyp icTyp depth fieldName 
     | (TyApp _ l r) <- icTyp 
     = let (maybeConName:fieldTypes) = unfoldAppCon icTyp
           cogFields = getFields cogIcTyp
@@ -444,20 +495,22 @@ analyseTypes cogIcTyp icTyp depth fieldName
           groupTag = case cogIcTyp of 
                        (CC.TRecord _ fs _) -> HsRecord
                        (CC.TSum alts) -> HsVariant
+          accessors = getAccessor conName groupTag fieldTypes (Just (map fst cogFields))
         in HsEmbedLayout l groupTag $ 
-            M.fromList [ ( (fst (cogFields!!i))
-                         , let f = cogFields!!i
-                             in Right (analyseTypes (fst (snd f)) (fieldTypes!!i) (depth+1) (fst f))
-                         )
+            M.fromList [ let a = accessors!!i
+                             f = cogFields!!i
+                           in ( a 
+                              , Right (determineUnpack (fst (snd f)) (fieldTypes!!i) (depth+1) a)
+                              )
                        | i <- [0..(P.length cogFields)-1]
                        ]
-analyseTypes cogIcTyp icTyp depth fieldName 
+determineUnpack cogIcTyp icTyp depth fieldName 
     | (TyList _ ty) <- icTyp 
     = let elemCogTy = case cogIcTyp of 
                      (CC.TArray t _ _ _) -> t
                      _ -> __impossible $ "Bad Abstraction"++" --> "++"Hs: "++show icTyp
-        in HsEmbedLayout icTyp HsList $ M.fromList [ ( "1" , Right (analyseTypes elemCogTy ty (depth+1) "1")) ]
-analyseTypes cogIcTyp icTyp depth fieldName 
+        in HsEmbedLayout icTyp HsList $ M.fromList [ ( "1" , Right (determineUnpack elemCogTy ty (depth+1) "1")) ]
+determineUnpack cogIcTyp icTyp depth fieldName 
     | _ <- icTyp 
     = __impossible $ "Bad Abstraction"++" --> "++"Hs: "++show icTyp
 
@@ -471,6 +524,47 @@ analyseTypes cogIcTyp icTyp depth fieldName
         (TyUnboxedSum _ tfs) -> __impossible "Compiler should never produce TyUnboxedSum embedding"
         otherwise -> __impossible "Bad abstraction"
 -}
+
+
+-- | determineUnpack'
+-- ---------------
+-- | Builds the structure used for generating the body of rrel
+-- ----------------------------------------------------------------------------------------------------
+determineUnpack' :: Type () -> Int -> String -> HsEmbedLayout
+determineUnpack' (TyParen _ t   ) depth fieldName = determineUnpack' t depth fieldName
+determineUnpack' hsTyp depth fieldName | (TyTuple _ _ tfs) <- hsTyp 
+    = HsEmbedLayout hsTyp HsTuple $ 
+        M.fromList [ let k = "_"++show (i+1)
+                       in (k , Right (determineUnpack' (tfs!!i) (depth+1) k))
+                   | i <- [0..(P.length tfs)-1]
+                   ]
+determineUnpack' hsTyp depth fieldName | (TyCon _ cn) <- hsTyp
+    = HsEmbedLayout hsTyp HsPrim $
+        M.fromList $ case (checkIsPrim cn) of 
+                          Just x -> [(fieldName, Left depth)] 
+                          Nothing -> []
+determineUnpack' hsTyp depth fieldName | (TyApp _ l r) <- hsTyp 
+    = let (maybeConName:fieldTypes) = unfoldAppCon hsTyp
+          conName = case maybeConName of 
+                      (TyCon _ (UnQual _ (Ident _ n))) -> n
+                      _ -> __impossible $ "Bad Constructor name"++show l++"--"++show r
+          groupTag = if | elem conName hsSumTypes -> HsVariant
+                        | otherwise -> HsRecord
+          -- TODO: unsure if this works - when would oa be a custom record?
+          --       Makes more sense to restrict oa to either a prim or a built in hs type rather then allowing
+          --       them to define there own records
+          fldNames = getAccessor conName groupTag fieldTypes Nothing
+        in HsEmbedLayout l groupTag $ 
+            M.fromList [ (  fldNames!!i
+                         , Right (determineUnpack' (fieldTypes!!i) (depth+1) (fldNames!!i))
+                         )
+                       | i <- [0..(P.length fieldTypes)-1]
+                       ]
+determineUnpack' hsTyp depth fieldName | (TyList _ ty) <- hsTyp 
+    = HsEmbedLayout hsTyp HsList $ 
+        M.fromList [ ( "1" , Right (determineUnpack' hsTyp (depth+1) "1")) ]
+determineUnpack' hsTyp depth fieldName | _ <- hsTyp = __impossible $ "Bad Abstraction"++" --> "++"Hs: "++show hsTyp
+
 
 -- Helpers
 -- ----------------------------------------------------------------------------------------------------
@@ -509,8 +603,26 @@ unfoldAppCon t = case t of
                    (TyCon _ n) -> [t]
                    _ -> [t]
 
+getAccessor :: String -> GroupTag -> [Type ()] -> Maybe [String] -> [String]
+getAccessor conName groupTag ts fieldNames
+    = let fs = case fieldNames of 
+                 Just x -> x 
+                 Nothing -> filter (/="None") . 
+                    (map (\x -> case x of 
+                                 (TyCon _ (UnQual _ (Ident _ n))) -> n
+                                 _ -> "None"
+                         )
+                    ) $ ts
+        in case groupTag of 
+            HsRecord -> fs
+            HsVariant -> if | conName == "Maybe" && P.length fs == 1 -> map (\x -> "_Just") fs 
+                            | conName == "Either" && P.length fs == 2 -> ["_Left", "_Right"]
+                            | otherwise -> map (\x -> "_" ++ conName ++ "_" ++ x) fs
+
 prims = ["Word8","Word16","Word32","Word64","Bool","String"]
         ++ ["Int"]
+
+hsSumTypes = ["Maybe","Either"]
 
 checkIsPrim :: QName () -> Maybe String
 checkIsPrim x = case x of 
@@ -526,10 +638,10 @@ isInt (TyCon _ (UnQual _ (Ident _ n))) = checkTy n ["Int"]
 
 isFromIntegral :: Type () -> Bool
 isFromIntegral (TyCon _ (UnQual _ (Ident _ n))) 
-    = any (==n) ["Word8","Word16","Word32","Word64"]
+    = elem n ["Word8","Word16","Word32","Word64"]
 
 checkTy :: String -> [String] -> Bool
-checkTy n xs = any (\x -> n == x) xs
+checkTy n xs = elem n xs
 
 rmdups :: (Ord a) => [a] -> [a]
 rmdups = map P.head . group . sort
@@ -555,8 +667,3 @@ getConNames tyL acc
 -- ----------------------------------------------------------------------------------------------------
 
 
--- | mkRrelBody 
--- | dummy func ATM
--- --------------------------------------------------
-mkRrelBody :: CC.Type t a -> Type () -> SG (Exp ())
-mkRrelBody concT absT = pure $ function $ "undefined"
