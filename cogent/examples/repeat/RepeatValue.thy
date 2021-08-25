7fdd6e7d3e99ac0e4dd98ee08df9a35ea4c058ab
Louis Cheung <louis.cheung1@unsw.edu.au>
2021-08-25 17:26:53 +1000

    File and proof cleanup
    Changes:
        * added some general helper lemmas
        * rearranged the layout of some files
        * added wrapper theorems about the repeat loop for
          preservation, mono-correspondence, and monomorphisation

--- cogent/examples/repeat/RepeatValue.thy
+++ cogent/examples/repeat/RepeatValue.thy
@@ -1,5 +1,5 @@
 theory RepeatValue
-  imports "build/Generated_AllRefine"
+  imports RepeatAssm
 begin
 
 fun vrepeat_bod
@@ -23,7 +23,31 @@ fun vvalfun_to_expr
 "vvalfun_to_expr (VAFunction f ts) = AFun f ts" |
 "vvalfun_to_expr _ = undefined"
 
-context shallow begin
+context value_sem begin
+
+definition vrepeat
+  where
+"vrepeat \<Xi>' \<xi>' \<tau>a \<tau>o x y =
+  (\<exists>n f g acc obsv ret.
+    x = VRecord [VPrim (LU64 n), f, g, acc, obsv] \<and>
+    y = ret \<and>
+    is_vvalfun f \<and>
+    is_vvalfun g \<and>
+    (\<Xi>' \<turnstile> acc :v \<tau>a) \<and>
+    (\<Xi>' \<turnstile> obsv :v \<tau>o) \<and>
+    (\<Xi>', [], [option.Some (TRecord [(''acc'', bang \<tau>a, Present), (''obsv'', \<tau>o, Present)] Unboxed)]
+      \<turnstile> (App (vvalfun_to_expr f) (Var 0)) : TPrim Bool) \<and>
+    (\<Xi>', [], [option.Some (TRecord [(''acc'', \<tau>a, Present), (''obsv'', \<tau>o, Present)] Unboxed)]
+      \<turnstile> (App (vvalfun_to_expr g) (Var 0)) : \<tau>a) \<and>
+    vrepeat_bod \<xi>' (unat n) (vvalfun_to_expr f) (vvalfun_to_expr g) acc obsv ret)"
+
+definition \<xi>m0 :: "'b \<Rightarrow> ('b, 'a) vval \<Rightarrow> ('b, 'a) vval \<Rightarrow> bool "
+  where
+"\<xi>m0 f x y = False"
+
+definition \<xi>m1 :: "funtyp \<Rightarrow> (funtyp, 'a) vval \<Rightarrow> (funtyp, 'a) vval \<Rightarrow> bool "
+  where
+"\<xi>m1 f x y = (if f = ''repeat_0'' then vrepeat \<Xi> \<xi>m0 abbreviatedType1 (TPrim (Num U64)) x y else \<xi>m0 f x y)"
 
 lemma vrepeat_bod_preservation:
   "\<lbrakk>proc_ctx_wellformed \<Xi>';
@@ -46,6 +70,80 @@ lemma vrepeat_bod_preservation:
    apply (rule v_t_r_cons1; simp?)+
    apply (rule v_t_r_empty)
   apply (drule meta_mp; simp)
+  done
+
+lemma vrepeat_bod_early_termination:
+  "\<lbrakk>vrepeat_bod \<xi>' i f g acc obsv ret; i < n;
+    \<xi>' , [VRecord [ret, obsv]] \<turnstile> App f (Var 0) \<Down> VPrim (LBool True)\<rbrakk>
+    \<Longrightarrow> vrepeat_bod \<xi>' n f g acc obsv ret"
+  apply (induct arbitrary: i rule: vrepeat_bod.induct[of _ \<xi>' n f g acc obsv ret])
+   apply clarsimp
+  apply clarsimp
+  apply (rename_tac i)
+  apply (case_tac i; clarsimp)
+   apply blast
+  apply (case_tac b; clarsimp)
+   apply blast
+  apply (rename_tac ret nat b acc')
+  apply (rule_tac x = b in exI)
+  apply (rule conjI; clarsimp)
+  apply (intro exI conjI; assumption)
+  done
+
+declare vrepeat_bod.simps[simp del]
+lemma vrepeat_bod_step:
+  "\<lbrakk>\<xi>', [VRecord [ret, obsv]] \<turnstile> App f (Var 0) \<Down> VPrim (LBool False);
+    \<not>(\<xi>', [VRecord [ret, obsv]] \<turnstile> App f (Var 0) \<Down> VPrim (LBool True));
+    \<xi>', [VRecord [ret, obsv]] \<turnstile> App g (Var 0) \<Down> ret';
+    vrepeat_bod \<xi>' n f g acc obsv ret\<rbrakk> 
+    \<Longrightarrow> vrepeat_bod \<xi>' (Suc n) f g acc obsv ret'"
+  apply (induct arbitrary: ret' rule: vrepeat_bod.induct[of _ \<xi>' n f g acc obsv ret])
+  apply (simp only: vrepeat_bod.simps)
+   apply (clarsimp simp: vrepeat_bod.simps)
+  apply clarsimp
+  apply (erule vrepeat_bod.elims; clarsimp)
+  apply (rename_tac ret' \<xi> n f g acc obsv ret b)
+  apply (case_tac b; clarsimp)
+  apply (drule_tac x = b in meta_spec; clarsimp)
+  apply (elim meta_allE meta_impE, assumption, assumption)
+  apply (subst vrepeat_bod.simps; clarsimp)
+  apply (rule_tac x = b in exI; clarsimp)
+  apply (intro exI conjI; assumption)
+  done
+declare vrepeat_bod.simps[simp]
+
+lemma vrepeat_preservation:
+  "\<And>v v'.
+       \<lbrakk>proc_ctx_wellformed \<Xi>';
+        \<xi>' matches \<Xi>';
+        \<Xi>' \<turnstile> v :v TRecord
+                  [(''n'', TPrim (Num U64), Present),
+                   (''stop'', TFun (TRecord [(''acc'', bang \<tau>a, Present), (''obsv'', \<tau>o, Present)] Unboxed) (TPrim Bool), Present),
+                   (''step'', TFun (TRecord [(''acc'', \<tau>a, Present), (''obsv'', \<tau>o, Present)] Unboxed) \<tau>a, Present),
+                   (''acc'', \<tau>a, Present), (''obsv'', \<tau>o, Present)]
+                  Unboxed;
+        vrepeat \<Xi>' \<xi>' \<tau>a \<tau>o v v'\<rbrakk>
+       \<Longrightarrow> \<Xi>' \<turnstile> v' :v \<tau>a"
+  unfolding vrepeat_def
+  apply clarsimp
+  apply (erule v_t_recordE; clarsimp)
+  apply (erule v_t_r_consE; clarsimp)+
+  apply (rule vrepeat_bod_preservation; simp)
+  done
+
+lemma \<xi>m0_matches_\<Xi>:
+  "\<xi>m0 matches \<Xi>'"
+  unfolding proc_env_matches_def \<xi>m0_def
+  by clarsimp
+
+lemma \<xi>m1_matches_\<Xi>:
+  "\<xi>m1 matches \<Xi>"
+  unfolding proc_env_matches_def \<xi>m1_def
+  apply clarsimp
+  apply (rule conjI; clarsimp)
+   apply (subst (asm) \<Xi>_def; clarsimp simp: repeat_0_type_def)
+   apply (rule vrepeat_preservation[OF proc_ctx_wellformed_\<Xi> \<xi>m0_matches_\<Xi>]; (simp add: abbreviated_type_defs)?)
+  apply (clarsimp simp: \<xi>m0_def)
   done
 
 end (* of context *)
