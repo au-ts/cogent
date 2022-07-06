ef0ad8757e96bf09cd128375e2542914aac235c4
Zilin Chen <zilin.chen@student.unsw.edu.au>
2022-07-06 19:26:15 +1000

    cogent: fix shallow Isabelle generation

--- cogent/src/Cogent/Isabelle/Shallow.hs
+++ cogent/src/Cogent/Isabelle/Shallow.hs
@@ -400,8 +400,7 @@ shallowExpr (TE _ (Truncate t e)) = do
   te@(TPrim pt) <- unfoldSynsShallowM $ exprType e
   TPrim pt'@(UInt small) <- unfoldSynsShallowM t
   e' <- shallowExpr e
-  return $ TermWithType (mkApp (mkId "ucast") [shallowPrimOp CS.BitAnd [e', mkInt (2^small - 1)]])
-                        (shallowPrimType pt')
+  return $ TermWithType (mkApp (mkId "ucast") [e']) (shallowPrimType pt')
 
 
 shallowAlt :: (Show b,Eq b) => (TagName,VarName,TypedExpr t v VarName b) -> SG b (Term,Term)
